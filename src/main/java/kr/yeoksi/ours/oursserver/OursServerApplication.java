ecb1b23c502c48bc4ba23a19f2e420d14c807941
Coldot <41678750+Coldot@users.noreply.github.com>
2023-07-02 00:42:35 +0900

    Initial Commit

---
+++ src/main/java/kr/yeoksi/ours/oursserver/OursServerApplication.java
@@ -0,0 +1,13 @@
+package kr.yeoksi.ours.oursserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OursServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OursServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/yeoksi/ours/oursserver/OursServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.yeoksi.ours.oursserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OursServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
