164cc11be903d1ab5bec5925b6f78f96057d51bb
sp.arc <pcpark98@naver.com>
2023-07-18 21:12:08 +0900

    OURS-154 도메인 설계

---
+++ src/main/java/kr/yeoksi/ours/oursserver/domain/User.java
@@ -0,0 +1,35 @@
+package kr.yeoksi.ours.oursserver.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@Table(name = "users")
+public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "user_index")
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @CreationTimestamp
+    @Column(name = "last_login_at")
+    private LocalDateTime lastLoginAt;
+}
