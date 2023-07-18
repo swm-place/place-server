efc5c1e312a28180baccd3a77c830da5a6f40d29
sp.arc <pcpark98@naver.com>
2023-07-19 02:06:38 +0900

    [OURS-154] feature: transportation 도메인 설계

---
+++ src/main/java/kr/yeoksi/ours/oursserver/domain/Transportation.java
@@ -0,0 +1,19 @@
+package kr.yeoksi.ours.oursserver.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Transportation {
+
+    @Id @GeneratedValue
+    @Column(name = "transportation_index")
+    private Long id;
+
+    private String name;
+}
