69b38cfff7f2b1cdad9694f84f3f604d9a9deb82
sp.arc <pcpark98@naver.com>
2023-07-21 01:18:36 +0900

    [OURS-103] feature: api 응답을 위한 Response 객체 생성

---
+++ src/main/java/kr/yeoksi/ours/oursserver/domain/Response.java
@@ -0,0 +1,20 @@
+package kr.yeoksi.ours.oursserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Response<T> {
+
+    private String resultCode;
+    private T result;
+
+    public static Response<Void> error(String resultCode) {
+        return new Response(resultCode, null);
+    }
+
+    public static <T> Response<T> success(T result) {
+        return new Response("Success", result);
+    }
+}
