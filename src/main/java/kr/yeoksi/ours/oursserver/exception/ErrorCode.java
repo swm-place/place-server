60376c663ad4d46f1bcdee6cdeb259198882bf83
sp.arc <pcpark98@naver.com>
2023-07-21 02:33:21 +0900

    [OURS-103] feature: 에러코드 세팅

---
+++ src/main/java/kr/yeoksi/ours/oursserver/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package kr.yeoksi.ours.oursserver.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+
+    DUPLICATED_USER(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다.");
+
+    private HttpStatus status;
+    private String message;
+}
