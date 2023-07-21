5b1009e5905b499bbe3e702c1868b77268a41146
sp.arc <pcpark98@naver.com>
2023-07-22 01:39:31 +0900

    [OURS-103] fix: 환경변수 세팅해서 에러 해결

--- src/test/java/kr/yeoksi/ours/oursserver/service/UserServiceTest.java
+++ src/test/java/kr/yeoksi/ours/oursserver/service/UserServiceTest.java
@@ -2,28 +2,27 @@ package kr.yeoksi.ours.oursserver.service;
 
 import kr.yeoksi.ours.oursserver.domain.User;
 import kr.yeoksi.ours.oursserver.exception.DuplicatedUserException;
-//import kr.yeoksi.ours.oursserver.repository.UserRepository;
+import kr.yeoksi.ours.oursserver.repository.UserRepository;
 import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.transaction.annotation.Transactional;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@Transactional
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class UserServiceTest {
 
-    //@Autowired UserService userService;
-    //@Autowired UserRepository userRepository;
+    @Autowired UserService userService;
+    @Autowired UserRepository userRepository;
 
     /**
      * 회원 가입
      */
-    /*
     @Test
     public void signUp() throws Exception {
 
@@ -33,16 +32,14 @@ public class UserServiceTest {
         user.setNickname("testNickname");
         user.setPhoneNumber("010-1234-5678");
 
-        //String savedUserId = userService.signUp(user);
+        String savedUserId = userService.signUp(user);
 
-        //assertEquals(user, userRepository.findById(savedUserId));
+        assertEquals(user, userRepository.findById(savedUserId));
     }
-     */
 
     /**
      * 중복 회원 예외 확인
      */
-    /*
     @Test(expected = DuplicatedUserException.class)
     public void 중복_회원가입_예외() throws Exception {
 
@@ -58,10 +55,9 @@ public class UserServiceTest {
         user2.setNickname("testNickname2");
         user2.setPhoneNumber("010-1234-5678");
 
-        //userService.signUp(user1);
-        //userService.signUp(user2); // 여기서 중복 회원 예외가 발생해야 함.
+        userService.signUp(user1);
+        userService.signUp(user2); // 여기서 중복 회원 예외가 발생해야 함.
 
         fail("중복 회원 예외가 발생해야 한다.");
     }
-     */
 }
