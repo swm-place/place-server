3a8b696758049eec6860656d8c07391d0d879c06
sp.arc <pcpark98@naver.com>
2023-07-22 01:55:25 +0900

    [OURS-103] chore: 스프링 시큐리티 필터 설정

--- src/main/java/kr/yeoksi/ours/oursserver/config/SecurityConfig.java
+++ src/main/java/kr/yeoksi/ours/oursserver/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package kr.yeoksi.ours.oursserver.config;
 
+import kr.yeoksi.ours.oursserver.security.HeaderFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +20,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new HeaderFilter(), AbstractPreAuthenticatedProcessingFilter.class)
                 .build();
     }
 }
---
+++ src/main/java/kr/yeoksi/ours/oursserver/security/HeaderFilter.java
@@ -0,0 +1,29 @@
+package kr.yeoksi.ours.oursserver.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * API Gateway로 부터 오는 요청의 헤더에 X-User-Uid와 X-User-Email이 유효한지 검증하는 필터
+ */
+public class HeaderFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String uid = request.getHeader("X-User-Uid");
+        String email = request.getHeader("X-User-Email");
+
+        // uid와 email에 대한 유효성 검사 로직
+        if(uid == null || uid.isEmpty() || email == null || email.isEmpty()) {
+            logger.debug("uid와 email에 대한 유효성 검사를 통과하지 못했습니다.");
+        }
+
+        // 유효성 검사가 성공하면 다음 필터로 넘기기
+        else filterChain.doFilter(request, response);
+    }
+}
