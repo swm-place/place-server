292a80da7f10605eddb58a0bd7bce646d67f55c3
Coldot <41678750+Coldot@users.noreply.github.com>
2023-07-23 23:40:34 +0900

    [OURS-177] chore: 개발 편의를 위해 인증 없이 Swagger UI에 접근할 수 있도록 허용

--- src/main/java/kr/yeoksi/ours/oursserver/config/SecurityConfig.java
+++ src/main/java/kr/yeoksi/ours/oursserver/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
@@ -16,11 +17,21 @@ import java.io.IOException;
 public class SecurityConfig {
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // MARK: 개발 편의를 위해 인증 없이 Swagger UI에 접근할 수 있도록 허용
+        // TODO: 배포 시 개발 서버에만 /swagger-ui 접근을 허용하고, 운영 서버에는 허용하지 않도록 수정
+        return (web) -> web.ignoring().requestMatchers("/swagger-ui/**", "/v3/api-docs/**");
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .addFilterBefore(new HeaderFilter(), AbstractPreAuthenticatedProcessingFilter.class)
-                .build();
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable);
+
+        http
+            .addFilterBefore(new HeaderFilter(), AbstractPreAuthenticatedProcessingFilter.class);
+
+        return http.build();
     }
 }
