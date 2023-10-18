59fb007ad31b1144600119e71825699bc5fb33ac
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-18 19:16:06 +0900

    [OURS-360] feature: declare `findById()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/port/out/CourseRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/out/CourseRepository.java
@@ -2,6 +2,9 @@ package kr.yeoksi.ours.oursserver.course.service.port.out;
 
 import kr.yeoksi.ours.oursserver.course.domain.Course;
 
+import java.util.Optional;
+
 public interface CourseRepository {
     public Course save(Course course);
+    public Optional<Course> findById(Long id);
 }
