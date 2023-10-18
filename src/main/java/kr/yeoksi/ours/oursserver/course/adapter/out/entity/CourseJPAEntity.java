351bf6bd655a31f943242a2ee50bc6f2fad1eaf5
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-18 19:00:55 +0900

    [OURS-360] feature: define `CourseJPAEntity`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/entity/CourseJPAEntity.java
@@ -0,0 +1,54 @@
+package kr.yeoksi.ours.oursserver.course.adapter.out.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import kr.yeoksi.ours.oursserver.others.domain.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+public class CourseJPAEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_index")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_index")
+    private User user;
+
+    @NotBlank
+    @Column(length = 50)
+    @Size(max = 50)
+    private String title;
+
+    @Column(name = "start_at")
+    private LocalDateTime startAt;
+
+    @Column(name = "end_at")
+    private LocalDateTime endAt;
+
+    @NotNull
+    @ColumnDefault("false")
+    @Column(name = "in_progress", columnDefinition = "TINYINT(1)")
+    private boolean inProgress;
+
+    @NotNull
+    @ColumnDefault("false")
+    @Column(name = "is_finished", columnDefinition = "TINYINT(1)")
+    private boolean isFinished;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
