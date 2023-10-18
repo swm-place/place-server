5d0bf1dfa46960a73d9d2563f5147817e490cd8a
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-18 20:48:14 +0900

    [OURS-360] feature: implement `save()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/CourseRepositoryImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/CourseRepositoryImpl.java
@@ -1,5 +1,7 @@
 package kr.yeoksi.ours.oursserver.course.adapter.out;
 
+import kr.yeoksi.ours.oursserver.course.adapter.out.entity.CourseJpaEntity;
+import kr.yeoksi.ours.oursserver.course.domain.Course;
 import kr.yeoksi.ours.oursserver.course.service.port.out.CourseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,9 @@ public class CourseRepositoryImpl implements CourseRepository {
 
     private final CourseJpaRepository courseJPARepository;
 
+    @Override
+    public Course save(Course course) {
+        CourseJpaEntity courseJpaEntity = CourseJpaEntity.from(course);
+        return courseJPARepository.save(courseJpaEntity).toCourse();
+    }
 }
