37a2b1e1dcb7cd9c724360e999a2d1bc8082c34e
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-18 17:34:31 +0900

    [OURS-360] feature: define domain object `Course`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/domain/Course.java
@@ -0,0 +1,20 @@
+package kr.yeoksi.ours.oursserver.course.domain;
+
+import kr.yeoksi.ours.oursserver.others.domain.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Getter @Setter
+public class Course {
+    private Long id;
+    private User user;
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private boolean inProgress;
+    private boolean isFinished;
+    private LocalDateTime createdAt;
+}
