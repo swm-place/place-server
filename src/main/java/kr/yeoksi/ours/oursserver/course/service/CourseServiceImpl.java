a05b4e9f60a4ccf4eaba7ec7747010a6718c3d48
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-18 18:57:13 +0900

    [OURS-360] feature: implement `create()`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/CourseServiceImpl.java
@@ -0,0 +1,42 @@
+package kr.yeoksi.ours.oursserver.course.service;
+
+import kr.yeoksi.ours.oursserver.course.domain.Course;
+import kr.yeoksi.ours.oursserver.course.service.port.in.CourseService;
+import kr.yeoksi.ours.oursserver.course.service.port.out.CourseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CourseServiceImpl implements CourseService {
+
+    private final CourseRepository courseRepository;
+
+    @Override
+    public Course create(Course course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Optional<Course> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Course> findAllByUserId(Long userId) {
+        return null;
+    }
+
+    @Override
+    public Course update(Course course) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
