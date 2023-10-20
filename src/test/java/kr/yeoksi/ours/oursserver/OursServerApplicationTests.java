c951e7d4442a3b7adf105698f745b84c98a5ee15
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-20 19:15:39 +0900

    [OURS-360] chore: add TODO statement `JUnit5 기반의 테스트로 전역 수정`

--- src/test/java/kr/yeoksi/ours/oursserver/OursServerApplicationTests.java
+++ src/test/java/kr/yeoksi/ours/oursserver/OursServerApplicationTests.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+// TODO: JUnit5 기반의 테스트로 전역 수정
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class OursServerApplicationTests {
