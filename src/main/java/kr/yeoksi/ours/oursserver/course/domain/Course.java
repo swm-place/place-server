95387c11b12e459ccdb02816e97c4f86bdcf8243
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-24 05:21:49 +0900

    [OURS-360] feature: implement `update()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/domain/Course.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/domain/Course.java
@@ -2,7 +2,10 @@ package kr.yeoksi.ours.oursserver.course.domain;
 
 import kr.yeoksi.ours.oursserver.others.domain.User;
 import lombok.*;
+import org.springframework.beans.BeanUtils;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,4 +32,27 @@ public class Course {
     private boolean isFinished;
 
     private LocalDateTime createdAt;
+
+
+    public void update(Course source) {
+        PropertyDescriptor[] sourcePds = BeanUtils.getPropertyDescriptors(source.getClass());
+
+        List<String> ignoreProperties = new ArrayList<>();
+
+        for (PropertyDescriptor sourcePd : sourcePds) {
+            Method readMethod = sourcePd.getReadMethod();
+            if (readMethod != null) {
+                try {
+                    Object value = readMethod.invoke(source);
+                    if (value == null) {
+                        ignoreProperties.add(sourcePd.getName());
+                    }
+                } catch (Throwable ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        }
+
+        BeanUtils.copyProperties(source, this, ignoreProperties.toArray(new String[0]));
+    }
 }
