b0b0f59249bd12b2b49332f00c3c89c6b547421e
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-24 02:13:03 +0900

    [OURS-360] feature: define `PlaceReference`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/request/PlaceReference.java
@@ -0,0 +1,19 @@
+package kr.yeoksi.ours.oursserver.course.adapter.in.request;
+
+
+import jakarta.validation.constraints.NotNull;
+import kr.yeoksi.ours.oursserver.others.domain.Place;
+import lombok.Data;
+
+
+@Data
+public class PlaceReference {
+
+    @NotNull private String id;
+
+    public Place toPlace() {
+        return Place.builder()
+                .id(id)
+                .build();
+    }
+}
