fdf362b1e7f33ddf052486ca155b3e3ebad7c4a2
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-24 20:13:33 +0900

    [OURS-360] refactor: `EntityUtils`을 이용해 업데이트 로직 분리

--- src/main/java/kr/yeoksi/ours/oursserver/course/domain/Course.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/domain/Course.java
@@ -1,11 +1,9 @@
 package kr.yeoksi.ours.oursserver.course.domain;
 
 import kr.yeoksi.ours.oursserver.others.domain.User;
+import kr.yeoksi.ours.oursserver.utils.EntityUtils;
 import lombok.*;
-import org.springframework.beans.BeanUtils;
 
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,24 +33,6 @@ public class Course {
 
 
     public void update(Course source) {
-        PropertyDescriptor[] sourcePds = BeanUtils.getPropertyDescriptors(source.getClass());
-
-        List<String> ignoreProperties = new ArrayList<>();
-
-        for (PropertyDescriptor sourcePd : sourcePds) {
-            Method readMethod = sourcePd.getReadMethod();
-            if (readMethod != null) {
-                try {
-                    Object value = readMethod.invoke(source);
-                    if (value == null) {
-                        ignoreProperties.add(sourcePd.getName());
-                    }
-                } catch (Throwable ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        }
-
-        BeanUtils.copyProperties(source, this, ignoreProperties.toArray(new String[0]));
+        EntityUtils.updateNotNullProperties(this, source);
     }
 }
