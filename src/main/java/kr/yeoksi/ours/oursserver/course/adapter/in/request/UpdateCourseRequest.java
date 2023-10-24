796ac12673b6080379719917b1bf025aecc2334a
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-24 22:48:27 +0900

    [OURS-360] feature: define `UpdateCourseRequest`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/request/UpdateCourseRequest.java
@@ -0,0 +1,35 @@
+package kr.yeoksi.ours.oursserver.course.adapter.in.request;
+
+import jakarta.validation.constraints.NotNull;
+import kr.yeoksi.ours.oursserver.course.domain.Course;
+import kr.yeoksi.ours.oursserver.course.domain.PlaceInCourse;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class UpdateCourseRequest extends CreateCourseRequest {
+
+    @NotNull private Long id;
+
+    @Override
+    public Course toCourse() {
+        List<PlaceInCourse> placesInCourse = new ArrayList<>(
+                this.getPlacesInCourse().stream()
+                        .map(PlacesInCourseRequest::toPlaceInCourse)
+                        .toList()
+        );
+
+        return Course.builder()
+                .id(id)
+                .title(this.getTitle())
+                .description(this.getDescription())
+                .placesInCourse(placesInCourse)
+                .startAt(getStartAt())
+                .endAt(getEndAt())
+                .build();
+    }
+}
