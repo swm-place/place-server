7083645dd4e31d3953fe2f066e6297439aebccb8
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-25 17:45:23 +0900

    [OURS-359] refactor: `CourseMagazine` 도메인 내용 제거

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/domain/CourseMagazine.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/domain/CourseMagazine.java
@@ -1,41 +1,5 @@
 package kr.yeoksi.ours.oursserver.magazine.domain;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import kr.yeoksi.ours.oursserver.others.domain.User;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.FetchType.*;
-
-@Entity
-@Getter @Setter
 public class CourseMagazine {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "course_magazine_index")
-    private Long id;
-
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "user_index")
-    private User user;
-
-    @NotBlank
-    @Column(length = 50)
-    @Size(max = 50)
-    private String title;
-
-    private String imgUrl;
-
-    @NotBlank
-    @Column(columnDefinition = "TEXT")
-    private String contents;
-
-    @CreationTimestamp
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
 }
