644a08d208289343d79801c6aea0543222af7f7f
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-26 23:30:25 +0900

    [OURS-382] feature: define `PlaceInCourseService`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/in/PlaceInCourseService.java
@@ -0,0 +1,20 @@
+package kr.yeoksi.ours.oursserver.course.service.port.in;
+
+import kr.yeoksi.ours.oursserver.course.domain.PlaceInCourse;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PlaceInCourseService {
+
+    PlaceInCourse append(PlaceInCourse placeInCourse);
+
+    PlaceInCourse update(PlaceInCourse placeInCourse);
+
+    void delete(PlaceInCourse placeInCourse);
+
+    Optional<PlaceInCourse> findById(Long id);
+
+    List<PlaceInCourse> findByCourseId(Long courseId);
+
+}
