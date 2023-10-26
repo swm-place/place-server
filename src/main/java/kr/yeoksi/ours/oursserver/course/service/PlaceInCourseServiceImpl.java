e1a74120836b044176bc8c0c58a3fd73247f5170
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-27 03:58:28 +0900

    [OURS-382] feature: implement `append()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/PlaceInCourseServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/PlaceInCourseServiceImpl.java
@@ -1,18 +1,46 @@
 package kr.yeoksi.ours.oursserver.course.service;
 
+import kr.yeoksi.ours.oursserver.course.domain.Course;
 import kr.yeoksi.ours.oursserver.course.domain.PlaceInCourse;
+import kr.yeoksi.ours.oursserver.course.exception.NotExistedCourseException;
+import kr.yeoksi.ours.oursserver.course.exception.PlaceWrongReferenceWithCourseException;
+import kr.yeoksi.ours.oursserver.course.service.port.in.CourseService;
 import kr.yeoksi.ours.oursserver.course.service.port.in.PlaceInCourseService;
+import kr.yeoksi.ours.oursserver.course.service.port.out.PlaceInCourseRepository;
+import kr.yeoksi.ours.oursserver.others.exception.NotExistedPlaceException;
+import kr.yeoksi.ours.oursserver.others.service.PlaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Service
+@RequiredArgsConstructor
 public class PlaceInCourseServiceImpl implements PlaceInCourseService {
+
+    private final PlaceInCourseRepository placeInCourseRepository;
+
+    private final CourseService courseService;
+    private final PlaceService placeService;
+
+
     @Override
     public PlaceInCourse append(PlaceInCourse placeInCourse, String userId) {
         // validate right course and owner
+        Course course = courseService.findById(placeInCourse.getCourseId(), userId)
+                .orElseThrow(NotExistedCourseException::new);
 
+        // validate right place and sync
+        try {
+            placeInCourse.setPlace(
+                    placeService.findById(placeInCourse.getPlace().getId()));
+        } catch (NotExistedPlaceException e) {
+            // TODO: Place 관련 예외 처리 ServiceException으로 통합
+            throw new PlaceWrongReferenceWithCourseException();
+        }
 
-        // validate right place
-        return null;
+        return placeInCourseRepository.save(placeInCourse);
     }
 
     @Override
