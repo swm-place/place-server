a4cdf804b16db169957fa3aa71bd95450addfc02
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-26 21:21:02 +0900

    [OURS-381] fix: 클라이언트에게 노출되면 안 되는 Place 관련 필드들 JsonIgnore 처리

--- src/main/java/kr/yeoksi/ours/oursserver/others/domain/Menu.java
+++ src/main/java/kr/yeoksi/ours/oursserver/others/domain/Menu.java
@@ -1,5 +1,6 @@
 package kr.yeoksi.ours.oursserver.others.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -17,6 +18,8 @@ public class Menu {
     @Column(name = "menu_index")
     private Long id;
 
+    // TODO: @JsonIgnore 대신 응답용 DTO 사용
+    @JsonIgnore
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "place_index")
     private Place place;
