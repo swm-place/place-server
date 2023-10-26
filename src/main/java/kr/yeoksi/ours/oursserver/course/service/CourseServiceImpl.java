a94258d66f6c40be227c8d6179757512d75b7913
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-26 23:24:20 +0900

    [OURS-382] chore: `update()` 관련 해결해야 할 부분 TODO Statement 추가

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/CourseServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/CourseServiceImpl.java
@@ -59,7 +59,11 @@ public class CourseServiceImpl implements CourseService {
         Course courseToUpdate = validateIsExistedAndGet(course.getId());
         validateOwnership(courseToUpdate, userId);
 
+        // TODO: PlaceInCourse의 소유권 validation
+
         courseToUpdate.update(course);
+
+        // TODO: placesInCourse의 일부 필드만 반환되는 문제 해결
         return courseRepository.save(courseToUpdate);
     }
 
