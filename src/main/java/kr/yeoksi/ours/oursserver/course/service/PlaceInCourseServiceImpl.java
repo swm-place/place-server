fd49564099b11a002c9ea0d3901d8903d08d365c
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-26 23:50:16 +0900

    [OURS-382] feature: create `PlaceInCourseServiceImpl`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/PlaceInCourseServiceImpl.java
@@ -0,0 +1,33 @@
+package kr.yeoksi.ours.oursserver.course.service;
+
+import kr.yeoksi.ours.oursserver.course.domain.PlaceInCourse;
+import kr.yeoksi.ours.oursserver.course.service.port.in.PlaceInCourseService;
+
+import java.util.List;
+
+public class PlaceInCourseServiceImpl implements PlaceInCourseService {
+    @Override
+    public PlaceInCourse append(PlaceInCourse placeInCourse) {
+        return null;
+    }
+
+    @Override
+    public PlaceInCourse update(PlaceInCourse placeInCourse) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public PlaceInCourse findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<PlaceInCourse> findByCourseId(Long courseId) {
+        return null;
+    }
+}
