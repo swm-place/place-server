8a2c019d45eb8f2b02183f7eaea2e29620c0cb38
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-27 03:35:04 +0900

    [OURS-382] fix: 전 메서드에 `userId` 추가

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/port/in/PlaceInCourseService.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/in/PlaceInCourseService.java
@@ -7,14 +7,14 @@ import java.util.Optional;
 
 public interface PlaceInCourseService {
 
-    PlaceInCourse append(PlaceInCourse placeInCourse);
+    PlaceInCourse append(PlaceInCourse placeInCourse, String userId);
 
-    PlaceInCourse update(PlaceInCourse placeInCourse);
+    PlaceInCourse update(PlaceInCourse placeInCourse, String userId);
 
-    void delete(Long id);
+    void delete(Long id, String userId);
 
-    PlaceInCourse findById(Long id);
+    PlaceInCourse findById(Long id, String userId);
 
-    List<PlaceInCourse> findByCourseId(Long courseId);
+    List<PlaceInCourse> findByCourseId(Long courseId, String userId);
 
 }
