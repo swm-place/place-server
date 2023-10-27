25a4439e93bec7ebc33dc18390ba1aa7a6a1a053
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-27 19:02:24 +0900

    [OURS-382] test: `id로_코스_내_장소를_조회할_수_있다`

--- src/test/java/kr/yeoksi/ours/oursserver/course/service/PlaceInCourseServiceIntegrationTest.java
+++ src/test/java/kr/yeoksi/ours/oursserver/course/service/PlaceInCourseServiceIntegrationTest.java
@@ -2,14 +2,12 @@ package kr.yeoksi.ours.oursserver.course.service;
 
 import kr.yeoksi.ours.oursserver.course.domain.Course;
 import kr.yeoksi.ours.oursserver.course.domain.PlaceInCourse;
-import kr.yeoksi.ours.oursserver.course.service.port.in.CourseService;
 import kr.yeoksi.ours.oursserver.course.service.port.in.PlaceInCourseService;
 import kr.yeoksi.ours.oursserver.course.service.port.out.CourseRepository;
 import kr.yeoksi.ours.oursserver.others.domain.Place;
 import kr.yeoksi.ours.oursserver.others.domain.User;
 import kr.yeoksi.ours.oursserver.others.repository.PlaceRepository;
 import kr.yeoksi.ours.oursserver.others.repository.UserRepository;
-import kr.yeoksi.ours.oursserver.others.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -284,10 +282,64 @@ public class PlaceInCourseServiceIntegrationTest {
     @Test
     public void id로_코스_내_장소를_조회할_수_있다() {
         // given
+        // configure user
+        User user = User.builder()
+                .id("test")
+                .email("test@yeoksi.com")
+                .nickname("test")
+                .build();
+        userRepository.save(user);
+
+        // configure places
+        Place place1 = Place.builder()
+                .id("test1")
+                .name("test1")
+                .category("test1")
+                .build();
+        Place place2 = Place.builder()
+                .id("test2")
+                .name("test2")
+                .category("test2")
+                .build();
+        place1.setId(placeRepository.save(place1));
+        place2.setId(placeRepository.save(place2));
+
+        // configure placesInCourse
+        List<PlaceInCourse> placesInCourse = new ArrayList<>();
+        placesInCourse.add(PlaceInCourse.builder()
+                .place(place1)
+                .day(1)
+                .order(1)
+                .startAt(null)
+                .timeRequired(0)
+                .transportationTime(0)
+                .build());
+        placesInCourse.add(PlaceInCourse.builder()
+                .place(place2)
+                .day(1)
+                .order(2)
+                .startAt(null)
+                .timeRequired(0)
+                .transportationTime(0)
+                .build());
+
+        // configure course
+        Course course = Course.builder()
+                .user(user)
+                .title("test")
+                .description("test")
+                .placesInCourse(placesInCourse)
+                .build();
+        course = courseRepository.save(course);
 
         // when
+        PlaceInCourse placeInCourseFound = placeInCourseService.getById(course.getPlacesInCourse().get(0).getId(), user.getId());
 
         // then
+        assertThat(placeInCourseFound.getPlace().getId()).isEqualTo(place1.getId());
+        assertThat(placeInCourseFound.getPlace().getName()).isEqualTo(place1.getName());
+        assertThat(placeInCourseFound.getPlace().getCategory()).isEqualTo(place1.getCategory());
+
     }
 
 }
