bf7bbcd86bf629f72366cc9674db7e9270350031
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-27 18:26:07 +0900

    [OURS-382] test: `코스_내_장소를_추가할_수_있다`

--- src/test/java/kr/yeoksi/ours/oursserver/course/service/PlaceInCourseServiceIntegrationTest.java
+++ src/test/java/kr/yeoksi/ours/oursserver/course/service/PlaceInCourseServiceIntegrationTest.java
@@ -1,12 +1,26 @@
 package kr.yeoksi.ours.oursserver.course.service;
 
+import kr.yeoksi.ours.oursserver.course.domain.Course;
+import kr.yeoksi.ours.oursserver.course.domain.PlaceInCourse;
+import kr.yeoksi.ours.oursserver.course.service.port.in.CourseService;
 import kr.yeoksi.ours.oursserver.course.service.port.in.PlaceInCourseService;
+import kr.yeoksi.ours.oursserver.course.service.port.out.CourseRepository;
+import kr.yeoksi.ours.oursserver.others.domain.Place;
+import kr.yeoksi.ours.oursserver.others.domain.User;
+import kr.yeoksi.ours.oursserver.others.repository.PlaceRepository;
+import kr.yeoksi.ours.oursserver.others.repository.UserRepository;
+import kr.yeoksi.ours.oursserver.others.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @RunWith(SpringRunner.class)
@@ -16,14 +30,62 @@ public class PlaceInCourseServiceIntegrationTest {
 
     @Autowired PlaceInCourseService placeInCourseService;
 
+    @Autowired UserRepository userRepository;
+    @Autowired PlaceRepository placeRepository;
+    @Autowired CourseRepository courseRepository;
+
 
     @Test
     public void 코스_내_장소를_추가할_수_있다() {
         // given
+        // configure user
+        User user = User.builder()
+                .id("test")
+                .email("test@yeoksi.com")
+                .nickname("test")
+                .build();
+        userRepository.save(user);
+
+        // configure places
+        Place place = Place.builder()
+                .id("test1")
+                .name("test1")
+                .category("test1")
+                .build();
+        place.setId(placeRepository.save(place));
+
+        // configure course
+        Course course = Course.builder()
+                .user(user)
+                .title("test")
+                .description("test")
+                .build();
+        courseRepository.save(course);
+
+        // configure placesInCourse
+        PlaceInCourse placeInCourse = PlaceInCourse.builder()
+                .courseId(course.getId())
+                .place(place)
+                .day(1)
+                .order(1)
+                .startAt(null)
+                .timeRequired(0)
+                .transportationTime(0)
+                .build();
 
         // when
+        PlaceInCourse saved = placeInCourseService.append(placeInCourse, user.getId());
 
         // then
+        assertThat(saved.getId()).isEqualTo(placeInCourse.getId());
+        assertThat(saved.getCourseId()).isEqualTo(placeInCourse.getCourseId());
+        assertThat(saved.getPlace().getId()).isEqualTo(placeInCourse.getPlace().getId());
+        assertThat(saved.getDay()).isEqualTo(placeInCourse.getDay());
+        assertThat(saved.getOrder()).isEqualTo(placeInCourse.getOrder());
+        assertThat(saved.getStartAt()).isEqualTo(placeInCourse.getStartAt());
+        assertThat(saved.getTimeRequired()).isEqualTo(placeInCourse.getTimeRequired());
+        assertThat(saved.getTransportationTime()).isEqualTo(placeInCourse.getTransportationTime());
+
     }
 
     @Test
