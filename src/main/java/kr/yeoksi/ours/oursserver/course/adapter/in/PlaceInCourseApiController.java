7176f756fba07d4f7716f812197af3353aba8d67
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-27 19:13:30 +0900

    [OURS-382] feature: implement `addPlaceToCourse()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/PlaceInCourseApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/PlaceInCourseApiController.java
@@ -1,9 +1,13 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in;
 
+import jakarta.validation.Valid;
+import kr.yeoksi.ours.oursserver.course.adapter.in.request.PlaceInCourseCreateRequest;
+import kr.yeoksi.ours.oursserver.course.adapter.in.response.PlaceInCourseResponse;
+import kr.yeoksi.ours.oursserver.course.domain.PlaceInCourse;
 import kr.yeoksi.ours.oursserver.course.service.port.in.PlaceInCourseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -14,5 +18,14 @@ public class PlaceInCourseApiController {
     private final PlaceInCourseService placeInCourseService;
 
 
-
+    @PostMapping("/{placeInCourseId}")
+    public ResponseEntity<PlaceInCourseResponse> addPlaceToCourse(@RequestHeader("X-User-Uid") String userId,
+                                                                  @PathVariable String courseId,
+                                                                  @PathVariable String placeInCourseId,
+                                                                  @RequestBody @Valid PlaceInCourseCreateRequest request) {
+        PlaceInCourse placeInCourse = request.toPlaceInCourse();
+        return ResponseEntity.ok(
+                PlaceInCourseResponse.from(
+                        placeInCourseService.append(placeInCourse, userId)));
+    }
 }
