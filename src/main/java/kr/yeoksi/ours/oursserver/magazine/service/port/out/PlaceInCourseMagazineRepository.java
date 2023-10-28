4e240de76b00d095d7d3b740110a685019008b00
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 04:45:23 +0900

    [OURS-359] feature: add parameter `courseMagazine`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/PlaceInCourseMagazineRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/PlaceInCourseMagazineRepository.java
@@ -1,12 +1,13 @@
 package kr.yeoksi.ours.oursserver.magazine.service.port.out;
 
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
 import kr.yeoksi.ours.oursserver.magazine.domain.PlaceInCourseMagazine;
 
 import java.util.List;
 
 public interface PlaceInCourseMagazineRepository {
 
-    PlaceInCourseMagazine save(PlaceInCourseMagazine placeInCourseMagazine);
+    PlaceInCourseMagazine save(PlaceInCourseMagazine placeInCourseMagazine, CourseMagazine courseMagazine);
 
     PlaceInCourseMagazine findById(Long id);
 
