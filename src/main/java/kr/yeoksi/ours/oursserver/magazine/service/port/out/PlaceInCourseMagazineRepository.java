21b51fcee4982bbb93c2b916f6ac7345a23c121e
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 04:17:02 +0900

    [OURS-359] feature: define `PlaceInCourseMagazineRepository`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/PlaceInCourseMagazineRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/PlaceInCourseMagazineRepository.java
@@ -1,4 +1,17 @@
 package kr.yeoksi.ours.oursserver.magazine.service.port.out;
 
+import kr.yeoksi.ours.oursserver.magazine.domain.PlaceInCourseMagazine;
+
+import java.util.List;
+
 public interface PlaceInCourseMagazineRepository {
+
+    PlaceInCourseMagazine save(PlaceInCourseMagazine placeInCourseMagazine);
+
+    PlaceInCourseMagazine findById(Long id);
+
+    List<PlaceInCourseMagazine> findByMagazineId(Long magazineId);
+
+    void delete(Long id);
+
 }
