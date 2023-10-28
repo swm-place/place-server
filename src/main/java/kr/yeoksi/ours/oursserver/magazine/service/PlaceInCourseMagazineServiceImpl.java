bdec1d859b3f17130faa7db144f0c9eb4955c2c4
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 05:25:34 +0900

    [OURS-359] feature: implement `append()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/PlaceInCourseMagazineServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/PlaceInCourseMagazineServiceImpl.java
@@ -1,8 +1,13 @@
 package kr.yeoksi.ours.oursserver.magazine.service;
 
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
 import kr.yeoksi.ours.oursserver.magazine.domain.PlaceInCourseMagazine;
+import kr.yeoksi.ours.oursserver.magazine.exception.PlaceWrongReferenceException;
+import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineService;
 import kr.yeoksi.ours.oursserver.magazine.service.port.in.PlaceInCourseMagazineService;
 import kr.yeoksi.ours.oursserver.magazine.service.port.out.PlaceInCourseMagazineRepository;
+import kr.yeoksi.ours.oursserver.others.exception.NotExistedPlaceException;
+import kr.yeoksi.ours.oursserver.others.service.PlaceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +20,28 @@ public class PlaceInCourseMagazineServiceImpl implements PlaceInCourseMagazineSe
 
     private final PlaceInCourseMagazineRepository placeInCourseMagazineRepository;
 
+    private final CourseMagazineService courseMagazineService;
+    private final PlaceService placeService;
+
 
     @Override
     public PlaceInCourseMagazine append(PlaceInCourseMagazine placeInCourseMagazine, Long magazineId, String userId) {
-        return null;
+        // validate right magazine and owner
+        CourseMagazine magazine = courseMagazineService.getById(magazineId);
+
+        // validate right place and sync place info
+        try {
+            placeInCourseMagazine.setPlace(
+                    placeService.findById(placeInCourseMagazine.getPlace().getId()));
+        }
+        catch (NotExistedPlaceException e) {
+            // TODO: Place 관련 예외 처리 ServiceException으로 통합
+            throw new PlaceWrongReferenceException();
+        }
+
+        // append placeInCourseMagazine
+        placeInCourseMagazine.setId(null);
+        return placeInCourseMagazineRepository.save(placeInCourseMagazine, magazine);
     }
 
     @Override
