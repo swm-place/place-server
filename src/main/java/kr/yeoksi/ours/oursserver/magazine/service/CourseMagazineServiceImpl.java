b0935230cd54d3b52a2fed04289c7db87616f13d
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 03:14:23 +0900

    [OURS-359] feature: implement `findLatestCourseMagazines()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineServiceImpl.java
@@ -57,9 +57,8 @@ public class CourseMagazineServiceImpl implements CourseMagazineService {
     }
 
     @Override
-    public List<CourseMagazine> findLatestCourseMagazines(int count) {
-        // TODO: implement
-        return null;
+    public List<CourseMagazine> findLatestCourseMagazines(int count, int page) {
+        return courseMagazineRepository.findPageOrderByCreatedAtDesc(count, page);
     }
 
     @Override
