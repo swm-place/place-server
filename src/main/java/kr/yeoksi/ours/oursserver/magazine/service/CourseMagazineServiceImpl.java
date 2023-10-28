197e1bdaa6324a1a86b2e7367445538835737144
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-28 22:35:26 +0900

    [OURS-359] feature: implement `publish()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineServiceImpl.java
@@ -1,12 +1,14 @@
 package kr.yeoksi.ours.oursserver.magazine.service;
 
 import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
+import kr.yeoksi.ours.oursserver.magazine.exception.DuplicatedCourseMagazineException;
 import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineService;
 import kr.yeoksi.ours.oursserver.magazine.service.port.out.CourseMagazineRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +16,19 @@ public class CourseMagazineServiceImpl implements CourseMagazineService {
 
     private CourseMagazineRepository courseMagazineRepository;
 
+
     @Override
     public CourseMagazine publish(CourseMagazine courseMagazine) {
-        return null;
+        // validate duplicated
+        Optional<CourseMagazine> found = courseMagazineRepository.findById(courseMagazine.getId());
+        if (found.isPresent()) {
+            throw new DuplicatedCourseMagazineException();
+        }
+
+        // place, user 연동
+
+        // save
+        return courseMagazineRepository.save(courseMagazine);
     }
 
     @Override
