e859fe1c2e267aa8bf566fe6213016a57ba81ee7
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-28 20:18:13 +0900

    [OURS-359] feature: define `PlaceInCourseMagazine`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/domain/PlaceInCourseMagazine.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/domain/PlaceInCourseMagazine.java
@@ -1,5 +1,20 @@
 package kr.yeoksi.ours.oursserver.magazine.domain;
 
+import kr.yeoksi.ours.oursserver.others.domain.Place;
+import lombok.Builder;
+import lombok.Data;
 
+
+@Data
+@Builder
 public class PlaceInCourseMagazine {
+
+    private Long id;
+    private CourseMagazine courseMagazine;
+
+    private Place place;
+
+    private String contents;
+    private int order;
+
 }
