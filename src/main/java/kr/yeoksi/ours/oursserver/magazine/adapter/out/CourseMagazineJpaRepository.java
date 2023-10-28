cddb4cff5792f49572ac05b88f6d441ec46158f6
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-28 17:27:38 +0900

    [OURS-359] fix: `CourseMagazineJpaRepository` needs `CourseMagazineJpaEntity`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/CourseMagazineJpaRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/CourseMagazineJpaRepository.java
@@ -1,7 +1,8 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.out;
 
+import kr.yeoksi.ours.oursserver.magazine.adapter.out.entity.CourseMagazineJpaEntity;
 import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseMagazineJpaRepository extends JpaRepository<CourseMagazine, Long> {
+public interface CourseMagazineJpaRepository extends JpaRepository<CourseMagazineJpaEntity, Long> {
 }
