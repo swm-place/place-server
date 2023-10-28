42d40ab2e45375f9dac759a2b1f07cb7d2a9fdf7
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 01:37:03 +0900

    [OURS-359] feature: `userId` 매개변수 추가

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/in/CourseMagazineService.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/in/CourseMagazineService.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public interface CourseMagazineService {
 
-    public CourseMagazine publish(CourseMagazine courseMagazine);
+    public CourseMagazine publish(CourseMagazine courseMagazine, Long userId);
 
     public CourseMagazine getById(Long id);
 
     public List<CourseMagazine> findLatestCourseMagazines(int count);
 
-    public CourseMagazine update(CourseMagazine courseMagazine);
+    public CourseMagazine update(CourseMagazine courseMagazine, Long userId);
 
-    public void delete(Long id);
+    public void delete(Long id, Long userId);
 
 }
