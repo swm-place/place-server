da2e9d067235c89a17132231c6287b93203517a2
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 05:12:06 +0900

    [OURS-359] feature: define `findByCourseMagazineId()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/PlaceInCourseMagazineJpaRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/PlaceInCourseMagazineJpaRepository.java
@@ -3,5 +3,10 @@ package kr.yeoksi.ours.oursserver.magazine.adapter.out;
 import kr.yeoksi.ours.oursserver.magazine.adapter.out.entity.PlaceInCourseMagazineJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlaceInCourseMagazineJpaRepository extends JpaRepository<PlaceInCourseMagazineJpaEntity, Long> {
+
+    List<PlaceInCourseMagazineJpaEntity> findByCourseMagazineId(Long magazineId);
+
 }
