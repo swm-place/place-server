7152bf166f4ef7a316653b443a6948ca50e344ac
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 05:09:45 +0900

    [OURS-359] fix: `findById()` should return `Optional<PlaceInCourseMagazine>` type

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/PlaceInCourseMagazineRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/PlaceInCourseMagazineRepository.java
@@ -4,12 +4,13 @@ import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
 import kr.yeoksi.ours.oursserver.magazine.domain.PlaceInCourseMagazine;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PlaceInCourseMagazineRepository {
 
     PlaceInCourseMagazine save(PlaceInCourseMagazine placeInCourseMagazine, CourseMagazine courseMagazine);
 
-    PlaceInCourseMagazine findById(Long id);
+    Optional<PlaceInCourseMagazine> findById(Long id);
 
     List<PlaceInCourseMagazine> findByMagazineId(Long magazineId);
 
