9eebc321763b98d2007d83fa940e597c6e689e8a
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 02:29:02 +0900

    [OURS-359] feature: `userId` should be `String` type

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/in/CourseMagazineService.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/in/CourseMagazineService.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public interface CourseMagazineService {
 
-    public CourseMagazine publish(CourseMagazine courseMagazine, Long userId);
+    public CourseMagazine publish(CourseMagazine courseMagazine, String userId);
 
     public CourseMagazine getById(Long id);
 
     public List<CourseMagazine> findLatestCourseMagazines(int count);
 
-    public CourseMagazine update(CourseMagazine courseMagazine, Long userId);
+    public CourseMagazine update(CourseMagazine courseMagazine, String userId);
 
-    public void delete(Long id, Long userId);
+    public void delete(Long id, String userId);
 
 }
