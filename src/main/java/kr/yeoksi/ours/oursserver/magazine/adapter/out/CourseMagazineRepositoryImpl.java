afe02f11c409abe9dc14ef57d0a32739933ff880
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 03:03:00 +0900

    [OURS-359] feature: inject dependencies

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/CourseMagazineRepositoryImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/CourseMagazineRepositoryImpl.java
@@ -1,4 +1,38 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.out;
 
-public class CourseMagazineRepositoryImpl {
+
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
+import kr.yeoksi.ours.oursserver.magazine.service.port.out.CourseMagazineRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class CourseMagazineRepositoryImpl implements CourseMagazineRepository {
+
+    private final CourseMagazineJpaRepository courseMagazineJpaRepository;
+
+    
+    @Override
+    public CourseMagazine save(CourseMagazine courseMagazine) {
+        return null;
+    }
+
+    @Override
+    public Optional<CourseMagazine> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<CourseMagazine> findLatestCourseMagazines(int count) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
