188206425efef53e1dd86420ff8799dec26eb64b
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 05:10:06 +0900

    [OURS-359] feature: implement `save()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/PlaceInCourseMagazineRepositoryImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/PlaceInCourseMagazineRepositoryImpl.java
@@ -1,17 +1,32 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.out;
 
+import kr.yeoksi.ours.oursserver.magazine.adapter.out.entity.CourseMagazineJpaEntity;
+import kr.yeoksi.ours.oursserver.magazine.adapter.out.entity.PlaceInCourseMagazineJpaEntity;
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
 import kr.yeoksi.ours.oursserver.magazine.domain.PlaceInCourseMagazine;
 import kr.yeoksi.ours.oursserver.magazine.service.port.out.PlaceInCourseMagazineRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
 @Repository
+@RequiredArgsConstructor
 public class PlaceInCourseMagazineRepositoryImpl implements PlaceInCourseMagazineRepository {
+
+    private PlaceInCourseMagazineJpaRepository placeInCourseMagazineJpaRepository;
+    private CourseMagazineJpaRepository courseMagazineJpaRepository;
+
+
     @Override
-    public PlaceInCourseMagazine save(PlaceInCourseMagazine placeInCourseMagazine) {
-        return null;
+    public PlaceInCourseMagazine save(PlaceInCourseMagazine placeInCourseMagazine, CourseMagazine courseMagazine) {
+        CourseMagazineJpaEntity courseMagazineJpaEntity = CourseMagazineJpaEntity.from(courseMagazine);
+        PlaceInCourseMagazineJpaEntity placeInCourseMagazineJpaEntity =
+                PlaceInCourseMagazineJpaEntity.from(placeInCourseMagazine, courseMagazineJpaEntity);
+
+        courseMagazineJpaEntity.addOrUpdatePlaceInCourseMagazine(placeInCourseMagazineJpaEntity);
+        return placeInCourseMagazineJpaRepository.save(placeInCourseMagazineJpaEntity).toPlaceInCourseMagazine();
     }
 
     @Override
