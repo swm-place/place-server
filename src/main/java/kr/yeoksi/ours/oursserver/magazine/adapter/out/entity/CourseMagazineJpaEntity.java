ea57fb0f81b06c8320cecd60e0959582e52e1cc4
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 05:17:06 +0900

    [OURS-359] feature: implement `removePlaceInCourseMagazine()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/entity/CourseMagazineJpaEntity.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/entity/CourseMagazineJpaEntity.java
@@ -97,4 +97,8 @@ public class CourseMagazineJpaEntity {
         }
     }
 
+    public void removePlaceInCourseMagazine(PlaceInCourseMagazineJpaEntity placeInCourseMagazine) {
+        placesInCourseMagazine.remove(placeInCourseMagazine);
+    }
+
 }
