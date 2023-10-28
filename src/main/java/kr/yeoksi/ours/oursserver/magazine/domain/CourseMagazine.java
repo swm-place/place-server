371b0ccc9d8825d0727e137fe7e3f3123d319cc4
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-28 20:17:13 +0900

    [OURS-359] feature: define `CourseMagazine`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/domain/CourseMagazine.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/domain/CourseMagazine.java
@@ -1,5 +1,28 @@
 package kr.yeoksi.ours.oursserver.magazine.domain;
 
 
+import kr.yeoksi.ours.oursserver.others.domain.User;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@Builder
 public class CourseMagazine {
+
+    private Long id;
+    private User user;
+
+    private String title;
+    private String contents;
+
+    @Builder.Default
+    private List<PlaceInCourseMagazine> placesInCourseMagazine = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+
 }
