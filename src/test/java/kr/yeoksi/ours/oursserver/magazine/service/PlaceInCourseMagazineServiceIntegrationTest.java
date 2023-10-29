db4d01ee49ec0c7960a95806eada88bd9656774b
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 21:45:59 +0900

    [OURS-359] test: `코스_매거진_내_장소를_삭제할_수_있다`

--- src/test/java/kr/yeoksi/ours/oursserver/magazine/service/PlaceInCourseMagazineServiceIntegrationTest.java
+++ src/test/java/kr/yeoksi/ours/oursserver/magazine/service/PlaceInCourseMagazineServiceIntegrationTest.java
@@ -162,4 +162,68 @@ public class PlaceInCourseMagazineServiceIntegrationTest {
         assertThat(found.getPlacesInCourseMagazine().get(0).getOrder()).isEqualTo(1);
 
     }
+
+    @Test
+    public void 코스_매거진_내_장소를_삭제할_수_있다() {
+        // given
+        // configure user
+        User user = User.builder()
+                .id("been")
+                .email("been@yeoksi.com")
+                .nickname("been")
+                .build();
+        userRepository.save(user);
+
+        // configure places
+        Place place1 = Place.builder()
+                .id("test1")
+                .name("test1")
+                .category("test1")
+                .build();
+        Place place2 = Place.builder()
+                .id("test2")
+                .name("test2")
+                .category("test2")
+                .build();
+        place1.setId(placeRepository.save(place1));
+        place2.setId(placeRepository.save(place2));
+
+        // configure placesInCourseMagazine
+        PlaceInCourseMagazine placeInCourseMagazine1 = PlaceInCourseMagazine.builder()
+                .place(place1)
+                .contents("test1")
+                .order(1)
+                .build();
+        PlaceInCourseMagazine placeInCourseMagazine2 = PlaceInCourseMagazine.builder()
+                .place(place2)
+                .contents("test2")
+                .order(2)
+                .build();
+        List<PlaceInCourseMagazine> placesInCourseMagazine = new ArrayList<>(
+                List.of(placeInCourseMagazine1, placeInCourseMagazine2)
+        );
+
+        // configure courseMagazine
+        CourseMagazine courseMagazine = CourseMagazine.builder()
+                .user(user)
+                .title("test")
+                .contents("test")
+                .placesInCourseMagazine(placesInCourseMagazine)
+                .build();
+        courseMagazineService.publish(courseMagazine, user.getId());
+
+
+        // when
+        placeInCourseMagazineService.delete(placeInCourseMagazine1.getId(), courseMagazine.getId(), user.getId());
+
+
+        // then
+        CourseMagazine found = courseMagazineService.getById(courseMagazine.getId());
+
+        assertThat(found.getPlacesInCourseMagazine().size()).isEqualTo(1);
+        assertThat(found.getPlacesInCourseMagazine().get(0).getPlace().getId()).isEqualTo(place2.getId());
+        assertThat(found.getPlacesInCourseMagazine().get(0).getContents()).isEqualTo("test2");
+        assertThat(found.getPlacesInCourseMagazine().get(0).getOrder()).isEqualTo(1);
+
+    }
 }
