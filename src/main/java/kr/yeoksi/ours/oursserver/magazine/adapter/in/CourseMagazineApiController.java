8880bdadb273e0ed08ea1db924d87cefbe7fb6a8
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 17:56:22 +0900

    [OURS-359] feature: implement `getLatestMagazines()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineApiController.java
@@ -1,12 +1,26 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.in;
 
+import kr.yeoksi.ours.oursserver.magazine.adapter.in.response.CourseMagazineResponse;
 import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/magazine/courses")
 @RequiredArgsConstructor
 public class CourseMagazineApiController {
     CourseMagazineService courseMagazineService;
+
+    @GetMapping
+    public ResponseEntity<List<CourseMagazineResponse>> getLatestMagazines(@RequestParam(defaultValue = "0") int page,
+                                                                           @RequestParam(defaultValue = "10") int count) {
+        return ResponseEntity.ok(
+                courseMagazineService.findLatestCourseMagazines(count, page)
+                        .stream().map(CourseMagazineResponse::from)
+                        .toList());
+    }
 
 }
