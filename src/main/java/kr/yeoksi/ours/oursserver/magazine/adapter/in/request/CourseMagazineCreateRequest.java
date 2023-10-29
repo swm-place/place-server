61bb3dcbfc32798e305a22a116c2964321b45579
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 20:23:23 +0900

    [OURS-359] feature: implement `toCourseMagazine()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/request/CourseMagazineCreateRequest.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/request/CourseMagazineCreateRequest.java
@@ -1,15 +1,35 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.in.request;
 
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
+import kr.yeoksi.ours.oursserver.magazine.domain.PlaceInCourseMagazine;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Data
 @Builder
 public class CourseMagazineCreateRequest {
+
     private String title;
     private String contents;
-    private List<PlaceInCourseMagazineCreateRequest> placesInCourseMagazine;
+
+    private List<PlaceInCourseMagazineCreateRequest> placesInCourseMagazine = new ArrayList<>();
+
+
+    public CourseMagazine toCourseMagazine() {
+        List<PlaceInCourseMagazine> placesInCourseMagazine = new ArrayList<>(
+            this.placesInCourseMagazine.stream()
+                .map(PlaceInCourseMagazineCreateRequest::toPlaceInCourseMagazine)
+                .toList()
+        );
+
+        return CourseMagazine.builder()
+                .title(title)
+                .contents(contents)
+                .placesInCourseMagazine(placesInCourseMagazine)
+                .build();
+    }
 }
