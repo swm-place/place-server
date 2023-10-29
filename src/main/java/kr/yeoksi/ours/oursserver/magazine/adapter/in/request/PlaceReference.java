8e45d3abf12a9982f663c13cff03f04d9e4ffd68
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 18:14:36 +0900

    [OURS-359] feature: define `PlaceReference`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/request/PlaceReference.java
@@ -0,0 +1,11 @@
+package kr.yeoksi.ours.oursserver.magazine.adapter.in.request;
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+public class PlaceReference {
+    private Long id;
+}
