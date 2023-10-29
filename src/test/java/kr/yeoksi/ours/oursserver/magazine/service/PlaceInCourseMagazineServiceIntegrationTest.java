cc324a5809ab1c87805d2d73bb4c46a046d6bb10
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-30 06:59:36 +0900

    [OURS-359] fix: publish된 매거진 재할당

--- src/test/java/kr/yeoksi/ours/oursserver/magazine/service/PlaceInCourseMagazineServiceIntegrationTest.java
+++ src/test/java/kr/yeoksi/ours/oursserver/magazine/service/PlaceInCourseMagazineServiceIntegrationTest.java
@@ -144,7 +144,7 @@ public class PlaceInCourseMagazineServiceIntegrationTest {
                 .contents("test")
                 .placesInCourseMagazine(placesInCourseMagazine)
                 .build();
-        courseMagazineService.publish(courseMagazine, user.getId());
+        courseMagazine = courseMagazineService.publish(courseMagazine, user.getId());
 
 
         // when
