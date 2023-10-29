faea8616dcac43903c666780c1163ee79f240a50
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 20:42:04 +0900

    [OURS-359] fix: reallocation base endpoint to `/course-magazines`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/magazine/courses")
+@RequestMapping("/course-magazines")
 @RequiredArgsConstructor
 public class CourseMagazineApiController {
 
