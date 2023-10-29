14db4373635c8d31abe133043e2ff7f5647edb04
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 20:33:51 +0900

    [OURS-359] feature: inject `PlaceInCourseMagazineService`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/PlaceInCourseMagazineApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/PlaceInCourseMagazineApiController.java
@@ -1,4 +1,17 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.in;
 
+
+import kr.yeoksi.ours.oursserver.magazine.service.port.in.PlaceInCourseMagazineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/magazine/courses/{courseId}/places")
+@RequiredArgsConstructor
 public class PlaceInCourseMagazineApiController {
+
+    private final PlaceInCourseMagazineService placeInCourseMagazineService;
+
+
 }
