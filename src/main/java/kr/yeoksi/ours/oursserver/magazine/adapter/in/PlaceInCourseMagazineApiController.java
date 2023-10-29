b73818078e6c0ab98bf4fdbfd03bd5358de50c32
Coldot <41678750+Coldot@users.noreply.github.com>
2023-10-29 20:37:46 +0900

    [OURS-359] feature: implement `getPlaceInCourseMagazine()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/PlaceInCourseMagazineApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/PlaceInCourseMagazineApiController.java
@@ -1,8 +1,12 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.in;
 
 
+import kr.yeoksi.ours.oursserver.magazine.adapter.in.response.PlaceInCourseMagazineResponse;
 import kr.yeoksi.ours.oursserver.magazine.service.port.in.PlaceInCourseMagazineService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +18,10 @@ public class PlaceInCourseMagazineApiController {
     private final PlaceInCourseMagazineService placeInCourseMagazineService;
 
 
+    @GetMapping("/{placeId}")
+    public ResponseEntity<PlaceInCourseMagazineResponse> getPlaceInCourseMagazine(@PathVariable Long courseId,
+                                                                                  @PathVariable Long placeId) {
+        return ResponseEntity.ok(PlaceInCourseMagazineResponse.from(
+                placeInCourseMagazineService.getById(placeId)));
+    }
 }
