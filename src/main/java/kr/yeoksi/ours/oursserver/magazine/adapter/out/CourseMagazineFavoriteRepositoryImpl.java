e3acc7fec13ec830e08276def7875a54853798eb
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 19:07:43 +0900

    [OURS-385] feature: define methods of `CourseMagazineFavoriteRepositoryImpl`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/CourseMagazineFavoriteRepositoryImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/CourseMagazineFavoriteRepositoryImpl.java
@@ -1,4 +1,28 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.out;
 
-public class CourseMagazineFavoriteRepositoryImpl {
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazineFavorite;
+import kr.yeoksi.ours.oursserver.magazine.service.port.out.CourseMagazineFavoriteRepository;
+
+import java.util.Optional;
+
+public class CourseMagazineFavoriteRepositoryImpl implements CourseMagazineFavoriteRepository {
+    @Override
+    public CourseMagazineFavorite save(CourseMagazineFavorite courseMagazineFavorite) {
+        return null;
+    }
+
+    @Override
+    public Optional<CourseMagazineFavorite> findByUserIdAndCourseMagazineId(Long userId, Long courseMagazineId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void delete(CourseMagazineFavorite courseMagazineFavorite) {
+
+    }
+
+    @Override
+    public boolean existsByUserIdAndCourseMagazineId(Long userId, Long courseMagazineId) {
+        return false;
+    }
 }
