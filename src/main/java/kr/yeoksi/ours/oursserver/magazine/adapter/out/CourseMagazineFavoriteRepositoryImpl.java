ee71807c3d8da18242371a379ef95bd42a8af101
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 19:08:07 +0900

    [OURS-385] feature: register repository bean

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/CourseMagazineFavoriteRepositoryImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/CourseMagazineFavoriteRepositoryImpl.java
@@ -2,9 +2,12 @@ package kr.yeoksi.ours.oursserver.magazine.adapter.out;
 
 import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazineFavorite;
 import kr.yeoksi.ours.oursserver.magazine.service.port.out.CourseMagazineFavoriteRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+@Repository
 public class CourseMagazineFavoriteRepositoryImpl implements CourseMagazineFavoriteRepository {
     @Override
     public CourseMagazineFavorite save(CourseMagazineFavorite courseMagazineFavorite) {
