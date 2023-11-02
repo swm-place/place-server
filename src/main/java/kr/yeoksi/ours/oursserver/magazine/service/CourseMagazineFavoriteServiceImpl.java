29a8ebc2969a28c178781aa71b3c8f95067eb9fc
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 18:51:40 +0900

    [OURS-385] feature: inject dependency and register bean

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineFavoriteServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineFavoriteServiceImpl.java
@@ -1,8 +1,18 @@
 package kr.yeoksi.ours.oursserver.magazine.service;
 
 import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineFavoriteService;
+import kr.yeoksi.ours.oursserver.magazine.service.port.out.CourseMagazineFavoriteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@RequiredArgsConstructor
 public class CourseMagazineFavoriteServiceImpl implements CourseMagazineFavoriteService {
+
+    private final CourseMagazineFavoriteRepository courseMagazineFavoriteRepository;
+
+
     @Override
     public void addFavorite(Long userId, Long courseMagazineId) {
 
