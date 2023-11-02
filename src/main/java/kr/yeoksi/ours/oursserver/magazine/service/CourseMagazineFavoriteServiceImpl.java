e016e981bfd342545d4baac6beb942e05ca1ce7e
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 19:04:07 +0900

    [OURS-385] feature: implement `addFavorite()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineFavoriteServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineFavoriteServiceImpl.java
@@ -1,5 +1,7 @@
 package kr.yeoksi.ours.oursserver.magazine.service;
 
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazineFavorite;
+import kr.yeoksi.ours.oursserver.magazine.exception.DuplicatedFavoriteException;
 import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineFavoriteService;
 import kr.yeoksi.ours.oursserver.magazine.service.port.out.CourseMagazineFavoriteRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +17,16 @@ public class CourseMagazineFavoriteServiceImpl implements CourseMagazineFavorite
 
     @Override
     public void addFavorite(Long userId, Long courseMagazineId) {
+        if (courseMagazineFavoriteRepository.existsByUserIdAndCourseMagazineId(userId, courseMagazineId)) {
+            throw new DuplicatedFavoriteException("이미 좋아하는 매거진입니다.");
+        }
 
+        courseMagazineFavoriteRepository.save(
+                CourseMagazineFavorite.builder()
+                        .userId(userId)
+                        .courseMagazineId(courseMagazineId)
+                        .build()
+        );
     }
 
     @Override
