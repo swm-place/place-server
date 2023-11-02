a18fff153f7b7761f7dfaffe2256e9ea6ceba527
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 21:59:40 +0900

    [OURS-385] feature: implement `addFavorite()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineFavoriteApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineFavoriteApiController.java
@@ -1,7 +1,11 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.in;
 
+import kr.yeoksi.ours.oursserver.magazine.exception.NoPermissionOfFavoriteException;
 import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineFavoriteService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,4 +15,14 @@ public class CourseMagazineFavoriteApiController {
 
     private final CourseMagazineFavoriteService courseMagazineFavoriteService;
 
+
+    @PostMapping("/favorites/{userId}/magazines/{courseMagazineId}")
+    public void addFavorite(@RequestHeader("X-User-Uid") String requestedUserId,
+                            @PathVariable String userId,
+                            @PathVariable Long courseMagazineId) {
+        if (!requestedUserId.equals(userId))
+            throw new NoPermissionOfFavoriteException();
+
+        courseMagazineFavoriteService.addFavorite(userId, courseMagazineId);
+    }
 }
