354beb5dc08f146649bb4aab066380a44a7de128
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 22:11:35 +0900

    [OURS-385] feature: define `findCourseMagazinesByUserId()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/CourseMagazineFavoriteRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/CourseMagazineFavoriteRepository.java
@@ -1,7 +1,9 @@
 package kr.yeoksi.ours.oursserver.magazine.service.port.out;
 
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
 import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazineFavorite;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CourseMagazineFavoriteRepository {
@@ -13,5 +15,7 @@ public interface CourseMagazineFavoriteRepository {
     void delete(CourseMagazineFavorite courseMagazineFavorite);
 
     boolean existsByUserIdAndCourseMagazineId(String userId, Long courseMagazineId);
+
+    List<CourseMagazine> findCourseMagazinesByUserId(String userId);
 
 }
