a6ab56472b72c8a5d6d67b482230840c72700c69
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 19:10:21 +0900

    [OURS-385] feature: create `CourseMagazineFavoriteJpaRepository`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/out/jpa/CourseMagazineFavoriteJpaRepository.java
@@ -0,0 +1,7 @@
+package kr.yeoksi.ours.oursserver.magazine.adapter.out.jpa;
+
+import kr.yeoksi.ours.oursserver.magazine.adapter.out.jpa.entity.CourseMagazineFavoriteJpaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseMagazineFavoriteJpaRepository extends JpaRepository<CourseMagazineFavoriteJpaEntity, Long> {
+}
