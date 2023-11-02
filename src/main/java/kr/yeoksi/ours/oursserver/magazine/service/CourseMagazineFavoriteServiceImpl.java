8466c5b7be3712e3a35e2ec2b520091b764a4183
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 18:37:23 +0900

    [OURS-385] feature: define `addFavorite()`, `deleteFavorite()`, `isFavorite()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineFavoriteServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/CourseMagazineFavoriteServiceImpl.java
@@ -1,4 +1,20 @@
 package kr.yeoksi.ours.oursserver.magazine.service;
 
-public class CourseMagazineFavoriteServiceImpl {
+import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineFavoriteService;
+
+public class CourseMagazineFavoriteServiceImpl implements CourseMagazineFavoriteService {
+    @Override
+    public void addFavorite(Long userId, Long courseMagazineId) {
+
+    }
+
+    @Override
+    public void deleteFavorite(Long userId, Long courseMagazineId) {
+
+    }
+
+    @Override
+    public boolean isFavorite(Long userId, Long courseMagazineId) {
+        return false;
+    }
 }
