1cf02a2280f6b43fd426d9a7f367c452611f449f
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 18:50:51 +0900

    [OURS-385] feature: define `CourseMagazineFavoriteRepository`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/CourseMagazineFavoriteRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/service/port/out/CourseMagazineFavoriteRepository.java
@@ -1,4 +1,17 @@
 package kr.yeoksi.ours.oursserver.magazine.service.port.out;
 
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazineFavorite;
+
+import java.util.Optional;
+
 public interface CourseMagazineFavoriteRepository {
+
+    CourseMagazineFavorite save(CourseMagazineFavorite courseMagazineFavorite);
+
+    Optional<CourseMagazineFavorite> findByUserIdAndCourseMagazineId(Long userId, Long courseMagazineId);
+
+    void delete(CourseMagazineFavorite courseMagazineFavorite);
+
+    boolean existsByUserIdAndCourseMagazineId(Long userId, Long courseMagazineId);
+
 }
