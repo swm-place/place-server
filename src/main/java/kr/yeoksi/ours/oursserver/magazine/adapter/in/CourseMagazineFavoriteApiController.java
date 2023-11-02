f92b9cc551139c4430d308da1565107e24ca25b9
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 21:24:53 +0900

    [OURS-385] feature: inject dependency

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineFavoriteApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineFavoriteApiController.java
@@ -1,8 +1,14 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.in;
 
+import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineFavoriteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 public class CourseMagazineFavoriteApiController {
+
+    private final CourseMagazineFavoriteService courseMagazineFavoriteService;
+
 }
