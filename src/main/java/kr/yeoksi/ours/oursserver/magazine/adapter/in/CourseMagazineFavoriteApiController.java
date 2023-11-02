1e733e20f816010fae5ad3f015d8baaed1d40edb
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 20:56:09 +0900

    [OURS-385] feature: create `CourseMagazineFavoriteApiController`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineFavoriteApiController.java
@@ -0,0 +1,8 @@
+package kr.yeoksi.ours.oursserver.magazine.adapter.in;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CourseMagazineFavoriteApiController {
+}
