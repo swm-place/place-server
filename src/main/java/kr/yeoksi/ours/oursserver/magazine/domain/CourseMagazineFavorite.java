30369b8c9fd6b8045086c58ea70959586ddc4c78
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 17:14:50 +0900

    [OURS-385] feature: define `CourseMagazineFavorite`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/domain/CourseMagazineFavorite.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/domain/CourseMagazineFavorite.java
@@ -1,5 +1,21 @@
 package kr.yeoksi.ours.oursserver.magazine.domain;
 
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Builder
 public class CourseMagazineFavorite {
+
+    private Long id;
+
+    private Long userId;
+    private Long courseMagazineId;
+
+    private LocalDateTime createdAt;
+
 }
