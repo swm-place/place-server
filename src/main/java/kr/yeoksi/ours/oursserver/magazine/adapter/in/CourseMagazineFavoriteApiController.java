df0385eccb3873fd2e0c87cefff7e07ebd89da01
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 22:00:05 +0900

    [OURS-385] feature: implement `deleteFavorite()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineFavoriteApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineFavoriteApiController.java
@@ -3,10 +3,7 @@ package kr.yeoksi.ours.oursserver.magazine.adapter.in;
 import kr.yeoksi.ours.oursserver.magazine.exception.NoPermissionOfFavoriteException;
 import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineFavoriteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -24,5 +21,15 @@ public class CourseMagazineFavoriteApiController {
             throw new NoPermissionOfFavoriteException();
 
         courseMagazineFavoriteService.addFavorite(userId, courseMagazineId);
+    }
+
+    @DeleteMapping("/favorites/{userId}/magazines/{courseMagazineId}")
+    public void deleteFavorite(@RequestHeader("X-User-Uid") String requestedUserId,
+                               @PathVariable String userId,
+                               @PathVariable Long courseMagazineId) {
+        if (!requestedUserId.equals(userId))
+            throw new NoPermissionOfFavoriteException();
+
+        courseMagazineFavoriteService.deleteFavorite(userId, courseMagazineId);
     }
 }
