639150fc463bc0f371e6ef791b9f1c90e41cf1b6
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-02 22:31:01 +0900

    [OURS-385] feature: implement `from()`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/response/FavoriteCourseMagazineResponse.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/response/FavoriteCourseMagazineResponse.java
@@ -1,9 +1,12 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.in.response;
 
+import kr.yeoksi.ours.oursserver.magazine.domain.CourseMagazine;
+import kr.yeoksi.ours.oursserver.magazine.domain.PlaceInCourseMagazine;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 
 
 @Data
@@ -15,4 +18,19 @@ public class FavoriteCourseMagazineResponse {
     private PlaceWithCourseMagazineResponse firstPlace;
 
     private LocalDateTime createdAt;
+
+
+    public static FavoriteCourseMagazineResponse from(CourseMagazine courseMagazine) {
+        return FavoriteCourseMagazineResponse.builder()
+                .id(courseMagazine.getId())
+                .title(courseMagazine.getTitle())
+                .firstPlace(PlaceWithCourseMagazineResponse.from(
+                        courseMagazine.getPlacesInCourseMagazine().stream()
+                                .sorted(Comparator.comparingInt(PlaceInCourseMagazine::getOrder))
+                                .toList().get(0)
+                                .getPlace())
+                )
+                .createdAt(courseMagazine.getCreatedAt())
+                .build();
+    }
 }
