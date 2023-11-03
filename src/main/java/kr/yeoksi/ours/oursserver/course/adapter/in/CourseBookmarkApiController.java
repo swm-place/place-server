8c3c3a741c0a31d5ad26e2401b63b57be688ce70
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 05:11:59 +0900

    [OURS-413] feature: implement `createCourseBookmark()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/CourseBookmarkApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/CourseBookmarkApiController.java
@@ -1,8 +1,14 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in;
 
+import jakarta.validation.Valid;
+import kr.yeoksi.ours.oursserver.course.adapter.in.request.CourseBookmarkCreateRequest;
+import kr.yeoksi.ours.oursserver.course.adapter.in.response.CourseBookmarkResponse;
+import kr.yeoksi.ours.oursserver.course.domain.CourseBookmark;
+import kr.yeoksi.ours.oursserver.course.exception.NoPermissionOfBookmarkException;
 import kr.yeoksi.ours.oursserver.course.service.port.in.CourseBookmarkService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -11,5 +17,18 @@ public class CourseBookmarkApiController {
 
     private final CourseBookmarkService courseBookmarkService;
 
+
+    @PostMapping("/bookmarks/{userId}/course-bookmarks")
+    public ResponseEntity<CourseBookmarkResponse> createCourseBookmark(@RequestHeader("X-User-Uid") String requestedUserId,
+                                                                       @PathVariable String userId,
+                                                                       @RequestBody @Valid CourseBookmarkCreateRequest request) {
+        if (!requestedUserId.equals(userId)) {
+            throw new NoPermissionOfBookmarkException();
+        }
+
+        return ResponseEntity.ok(
+                CourseBookmarkResponse.from(
+                        courseBookmarkService.createCourseBookmark(request.toCourseBookmark(), userId)));
+    }
 
 }
