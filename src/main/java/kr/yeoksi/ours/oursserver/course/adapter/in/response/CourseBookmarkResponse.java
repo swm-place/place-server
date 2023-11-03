81d98081b8d089582032a80d50518c9b7db3ed31
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 04:21:16 +0900

    [OURS-413] feature: apply `Data`, `Builder`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/response/CourseBookmarkResponse.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/response/CourseBookmarkResponse.java
@@ -1,8 +1,14 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in.response;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+
+@Data
+@Builder
 public class CourseBookmarkResponse {
 
     private Long id;
