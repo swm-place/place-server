c97e74859c8ec0837bd4f99178ed86a4daeb980b
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 02:07:37 +0900

    [OURS-413] feature: register repository bean

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/CourseBookmarkRepositoryImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/CourseBookmarkRepositoryImpl.java
@@ -2,10 +2,13 @@ package kr.yeoksi.ours.oursserver.course.adapter.out;
 
 import kr.yeoksi.ours.oursserver.course.domain.CourseBookmark;
 import kr.yeoksi.ours.oursserver.course.service.port.out.CourseBookmarkRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+
+@Repository
 public class CourseBookmarkRepositoryImpl implements CourseBookmarkRepository {
     @Override
     public CourseBookmark save(CourseBookmark courseBookmark) {
