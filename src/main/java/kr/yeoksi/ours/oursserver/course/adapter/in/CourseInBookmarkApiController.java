9461f0c668effe7bc2eda99a452d66d7ad45974e
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 05:35:50 +0900

    [OURS-413] feature: implement `addCourseToBookmark()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/CourseInBookmarkApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/CourseInBookmarkApiController.java
@@ -1,7 +1,11 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in;
 
+import kr.yeoksi.ours.oursserver.course.exception.NoPermissionOfBookmarkException;
 import kr.yeoksi.ours.oursserver.course.service.port.in.CourseInBookmarkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,4 +16,15 @@ public class CourseInBookmarkApiController {
     private final CourseInBookmarkService courseInBookmarkService;
 
 
+    @PostMapping("/bookmarks/{userId}/course-bookmarks/{courseBookmarkId}/courses/{courseId}")
+    public void addCourseToBookmark(@RequestHeader("X-User-Uid") String requestedUserId,
+                                    @PathVariable String userId,
+                                    @PathVariable Long courseBookmarkId,
+                                    @PathVariable Long courseId) {
+        if (!requestedUserId.equals(userId)) {
+            throw new NoPermissionOfBookmarkException();
+        }
+
+        courseInBookmarkService.addCourseToBookmark(courseBookmarkId, courseId, userId);
+    }
 }
