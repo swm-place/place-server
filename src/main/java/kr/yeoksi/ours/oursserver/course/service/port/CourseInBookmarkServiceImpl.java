896972a3267499678295db67c687ec1a01a6d9bd
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 00:54:17 +0900

    [OURS-413] feature: define `CourseInBookmarkServiceImpl`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/CourseInBookmarkServiceImpl.java
@@ -0,0 +1,15 @@
+package kr.yeoksi.ours.oursserver.course.service.port;
+
+import kr.yeoksi.ours.oursserver.course.service.port.in.CourseInBookmarkService;
+
+public class CourseInBookmarkServiceImpl implements CourseInBookmarkService {
+    @Override
+    public void addCourseToBookmark(Long courseBookmarkId, Long courseId, String userId) {
+
+    }
+
+    @Override
+    public void deleteCourseInBookmark(Long courseBookmarkId, Long courseId, String userId) {
+
+    }
+}
