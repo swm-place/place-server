958f12f735e152edf17276e861acb73b0ae77c43
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-03 22:24:46 +0900

    [OURS-413] feature: define `CourseInBookmark`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/domain/CourseInBookmark.java
@@ -0,0 +1,10 @@
+package kr.yeoksi.ours.oursserver.course.domain;
+
+public class CourseInBookmark {
+
+    private Long id;
+    private CourseBookmark courseBookmark;
+    private Course course;
+    private String createdAt;
+
+}
