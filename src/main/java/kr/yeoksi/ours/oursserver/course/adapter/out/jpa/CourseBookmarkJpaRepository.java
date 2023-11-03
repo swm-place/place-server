260d16608d009f7c9b031b6a0156b37b2ce39734
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 03:23:09 +0900

    [OURS-413] feature: implement `findByUserId()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/jpa/CourseBookmarkJpaRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/jpa/CourseBookmarkJpaRepository.java
@@ -1,12 +1,13 @@
 package kr.yeoksi.ours.oursserver.course.adapter.out.jpa;
 
 import kr.yeoksi.ours.oursserver.course.adapter.out.jpa.entity.CourseBookmarkJpaEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CourseBookmarkJpaRepository extends JpaRepository<CourseBookmarkJpaEntity, Long> {
 
-    List<CourseBookmarkJpaEntity> findByUserId(String userId);
+    List<CourseBookmarkJpaEntity> findByUserId(String userId, Pageable pageable);
 
 }
