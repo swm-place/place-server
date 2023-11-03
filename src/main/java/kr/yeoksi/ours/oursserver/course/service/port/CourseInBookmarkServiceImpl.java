e929cd3ca9610dc84260a8fb6e7ca74a0ef0de8a
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 01:29:03 +0900

    [OURS-413] fix: 중복 추가 검증 로직 추가

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/port/CourseInBookmarkServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/CourseInBookmarkServiceImpl.java
@@ -3,6 +3,7 @@ package kr.yeoksi.ours.oursserver.course.service.port;
 import kr.yeoksi.ours.oursserver.course.domain.Course;
 import kr.yeoksi.ours.oursserver.course.domain.CourseBookmark;
 import kr.yeoksi.ours.oursserver.course.domain.CourseInBookmark;
+import kr.yeoksi.ours.oursserver.course.exception.DuplicatedBookmarkException;
 import kr.yeoksi.ours.oursserver.course.service.port.in.CourseBookmarkService;
 import kr.yeoksi.ours.oursserver.course.service.port.in.CourseInBookmarkService;
 import kr.yeoksi.ours.oursserver.course.service.port.out.CourseInBookmarkRepository;
@@ -23,6 +24,10 @@ public class CourseInBookmarkServiceImpl implements CourseInBookmarkService {
     public void addCourseToBookmark(Long courseBookmarkId, Long courseId, String userId) {
         // validate permission
         CourseBookmark courseBookmark = courseBookmarkService.getCourseBookmark(courseBookmarkId, userId);
+
+        if (courseInBookmarkRepository.existsByCourseBookmarkIdAndCourseId(courseBookmark.getId(), courseId)) {
+            throw new DuplicatedBookmarkException("이미 해당 북마크에 추가된 코스입니다.");
+        }
 
         CourseInBookmark courseInBookmark = CourseInBookmark.builder()
                 .courseBookmark(courseBookmark)
