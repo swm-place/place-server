bf4ce68330580359912580bc30d2ebedf741dc0a
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-03 22:47:39 +0900

    [OURS-413] feature: define `getMyCourseBookmarks()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/port/in/CourseBookmarkService.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/in/CourseBookmarkService.java
@@ -2,11 +2,15 @@ package kr.yeoksi.ours.oursserver.course.service.port.in;
 
 import kr.yeoksi.ours.oursserver.course.domain.CourseBookmark;
 
+import java.util.List;
+
 public interface CourseBookmarkService {
 
     CourseBookmark createCourseBookmark(CourseBookmark courseBookmark);
 
     CourseBookmark getCourseBookmark(Long courseBookmarkId);
+
+    List<CourseBookmark> getMyCourseBookmarks(Long userId, int page, int size);
 
     CourseBookmark updateCourseBookmark(CourseBookmark courseBookmark);
 
