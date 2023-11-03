b6aa88fafc13db0c772835a8524726afe9b7d7a9
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 00:55:12 +0900

    [OURS-413] feature: inject dependency `courseInBookmarkRepository`

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/port/CourseInBookmarkServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/CourseInBookmarkServiceImpl.java
@@ -1,11 +1,18 @@
 package kr.yeoksi.ours.oursserver.course.service.port;
 
 import kr.yeoksi.ours.oursserver.course.service.port.in.CourseInBookmarkService;
+import kr.yeoksi.ours.oursserver.course.service.port.out.CourseInBookmarkRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@RequiredArgsConstructor
 public class CourseInBookmarkServiceImpl implements CourseInBookmarkService {
+
+    private final CourseInBookmarkRepository courseInBookmarkRepository;
+
+
     @Override
     public void addCourseToBookmark(Long courseBookmarkId, Long courseId, String userId) {
 
