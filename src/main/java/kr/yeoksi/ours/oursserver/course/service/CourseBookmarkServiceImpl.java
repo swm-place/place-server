cd416faf919d59cfdc0c49732807551909b6f4a9
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-03 23:29:43 +0900

    [OURS-413] feature: create `CourseBookmarkServiceImpl`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/CourseBookmarkServiceImpl.java
@@ -0,0 +1,33 @@
+package kr.yeoksi.ours.oursserver.course.service;
+
+import kr.yeoksi.ours.oursserver.course.domain.CourseBookmark;
+import kr.yeoksi.ours.oursserver.course.service.port.in.CourseBookmarkService;
+
+import java.util.List;
+
+public class CourseBookmarkServiceImpl implements CourseBookmarkService {
+    @Override
+    public CourseBookmark createCourseBookmark(CourseBookmark courseBookmark, String userId) {
+        return null;
+    }
+
+    @Override
+    public CourseBookmark getCourseBookmark(Long courseBookmarkId, String userId) {
+        return null;
+    }
+
+    @Override
+    public List<CourseBookmark> getMyCourseBookmarks(String userId, int page, int size) {
+        return null;
+    }
+
+    @Override
+    public CourseBookmark updateCourseBookmark(CourseBookmark courseBookmark, String userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteCourseBookmark(Long courseBookmarkId, String userId) {
+
+    }
+}
