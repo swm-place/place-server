e973ebddd4d427547e7bd11675102a01f498b5ff
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-03 22:08:18 +0900

    [OURS-413] feature: define `CourseInBookmarkJpaEntity`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/entity/CourseInBookmarkJpaEntity.java
@@ -0,0 +1,31 @@
+package kr.yeoksi.ours.oursserver.course.adapter.out.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+
+@Entity(name = "course_in_bookmark")
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CourseInBookmarkJpaEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_in_bookmark_index")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_bookmark_index")
+    private CourseBookmarkJpaEntity courseBookmark;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_index")
+    private CourseJpaEntity course;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private String createdAt;
+}
