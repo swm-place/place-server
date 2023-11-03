a9672f1d2a0e4e874d5ac6e47bbe10714b8e6433
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 04:09:43 +0900

    [OURS-413] feature: implement `toCourseBookmark()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/request/CourseBookmarkCreateRequest.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/request/CourseBookmarkCreateRequest.java
@@ -1,5 +1,6 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in.request;
 
+import kr.yeoksi.ours.oursserver.course.domain.CourseBookmark;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,5 +11,13 @@ public class CourseBookmarkCreateRequest {
 
     private Long id;
     private String title;
+
+
+    public CourseBookmark toCourseBookmark() {
+        return CourseBookmark.builder()
+                .id(id)
+                .title(title)
+                .build();
+    }
 
 }
