040a4e4b0124b7b64a552afde6bdf818d1042679
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 01:12:02 +0900

    [OURS-413] fix: `findById()` should return `Optional<CourseInBookmark>`

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/port/out/CourseInBookmarkRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/out/CourseInBookmarkRepository.java
@@ -2,11 +2,13 @@ package kr.yeoksi.ours.oursserver.course.service.port.out;
 
 import kr.yeoksi.ours.oursserver.course.domain.CourseInBookmark;
 
+import java.util.Optional;
+
 public interface CourseInBookmarkRepository {
 
     CourseInBookmark save(CourseInBookmark courseInBookmark);
 
-    CourseInBookmark findById(Long courseInBookmarkId);
+    Optional<CourseInBookmark> findById(Long courseInBookmarkId);
 
     CourseInBookmark findByCourseBookmarkIdAndCourseId(Long courseBookmarkId, Long courseId);
 
