bb0c3a18c72a6ba4e863181ab371501b056d23a5
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 01:37:54 +0900

    [OURS-413] feature: define `findByCourseId()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/port/in/CourseInBookmarkService.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/in/CourseInBookmarkService.java
@@ -1,9 +1,15 @@
 package kr.yeoksi.ours.oursserver.course.service.port.in;
 
+import kr.yeoksi.ours.oursserver.course.domain.CourseInBookmark;
+
+import java.util.List;
+
 public interface CourseInBookmarkService {
 
     void addCourseToBookmark(Long courseBookmarkId, Long courseId, String userId);
 
     void deleteCourseInBookmark(Long courseBookmarkId, Long courseId, String userId);
+
+    List<CourseInBookmark> findByCourseId(Long courseId, String userId);
 
 }
