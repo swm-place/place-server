dc8e52a5fb07cfb61791ddab30bd2cb24842a769
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 01:31:40 +0900

    [OURS-413] fix: set message internal

--- src/main/java/kr/yeoksi/ours/oursserver/course/exception/DuplicatedBookmarkException.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/exception/DuplicatedBookmarkException.java
@@ -4,7 +4,7 @@ import kr.yeoksi.ours.oursserver.exception.domain.ServiceException;
 import org.springframework.http.HttpStatus;
 
 public class DuplicatedBookmarkException extends ServiceException {
-    public DuplicatedBookmarkException(String message) {
-        super(HttpStatus.BAD_REQUEST, message);
+    public DuplicatedBookmarkException() {
+        super(HttpStatus.BAD_REQUEST, "이미 해당 북마크에 추가된 코스입니다.");
     }
 }
