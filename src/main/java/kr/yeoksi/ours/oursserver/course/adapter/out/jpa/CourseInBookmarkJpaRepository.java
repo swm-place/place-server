4cf7de9cdab6bdc8c58d19fce131b10afb306ba6
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 03:43:58 +0900

    [OURS-413] feature: define `findByCourseBookmarkIdAndCourseId()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/jpa/CourseInBookmarkJpaRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/jpa/CourseInBookmarkJpaRepository.java
@@ -3,5 +3,10 @@ package kr.yeoksi.ours.oursserver.course.adapter.out.jpa;
 import kr.yeoksi.ours.oursserver.course.adapter.out.jpa.entity.CourseInBookmarkJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseInBookmarkJpaRepository extends JpaRepository<CourseInBookmarkJpaEntity, Long> {
+
+    List<CourseInBookmarkJpaEntity> findByCourseBookmarkIdAndCourseId(Long courseBookmarkId, Long courseId);
+
 }
