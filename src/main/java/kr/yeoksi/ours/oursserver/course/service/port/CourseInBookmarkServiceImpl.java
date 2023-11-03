4fd316efdbad77d77928f453a0758d5012a8c4d5
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 00:54:29 +0900

    [OURS-413] feature: register service bean

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/port/CourseInBookmarkServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/port/CourseInBookmarkServiceImpl.java
@@ -1,7 +1,10 @@
 package kr.yeoksi.ours.oursserver.course.service.port;
 
 import kr.yeoksi.ours.oursserver.course.service.port.in.CourseInBookmarkService;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class CourseInBookmarkServiceImpl implements CourseInBookmarkService {
     @Override
     public void addCourseToBookmark(Long courseBookmarkId, Long courseId, String userId) {
