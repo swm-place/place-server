0b2c1d90f14d9115f0dcd4837fd1b04430d3dfb8
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 02:06:48 +0900

    [OURS-413] feature: define `CourseBookmarkRepositoryImpl`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/CourseBookmarkRepositoryImpl.java
@@ -0,0 +1,29 @@
+package kr.yeoksi.ours.oursserver.course.adapter.out;
+
+import kr.yeoksi.ours.oursserver.course.domain.CourseBookmark;
+import kr.yeoksi.ours.oursserver.course.service.port.out.CourseBookmarkRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CourseBookmarkRepositoryImpl implements CourseBookmarkRepository {
+    @Override
+    public CourseBookmark save(CourseBookmark courseBookmark) {
+        return null;
+    }
+
+    @Override
+    public Optional<CourseBookmark> findById(Long courseBookmarkId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<CourseBookmark> findByUserId(String userId, int page, int size) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long courseBookmarkId) {
+
+    }
+}
