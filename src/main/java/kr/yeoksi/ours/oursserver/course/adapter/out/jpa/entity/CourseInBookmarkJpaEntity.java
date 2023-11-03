97b30344d9f27c89ee0141d772fea9b4b9a68e74
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 03:30:53 +0900

    [OURS-413] feature: implement `toCourseInBookmark()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/jpa/entity/CourseInBookmarkJpaEntity.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/jpa/entity/CourseInBookmarkJpaEntity.java
@@ -1,6 +1,7 @@
 package kr.yeoksi.ours.oursserver.course.adapter.out.jpa.entity;
 
 import jakarta.persistence.*;
+import kr.yeoksi.ours.oursserver.course.domain.CourseInBookmark;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -30,5 +31,15 @@ public class CourseInBookmarkJpaEntity {
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+
+    public CourseInBookmark toCourseInBookmark() {
+        return CourseInBookmark.builder()
+                .id(this.id)
+                .courseBookmark(this.courseBookmark.toBookmark())
+                .course(this.course.toCourse())
+                .createdAt(this.createdAt)
+                .build();
+    }
 
 }
