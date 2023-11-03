cda9ad67356e71b1f83eaee385587dfa154bbdaf
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 05:22:21 +0900

    [OURS-413] feature: register controller bean

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/CourseInBookmarkApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/CourseInBookmarkApiController.java
@@ -1,4 +1,8 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in;
 
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class CourseInBookmarkApiController {
 }
