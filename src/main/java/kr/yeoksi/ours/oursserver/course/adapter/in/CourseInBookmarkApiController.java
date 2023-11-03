c638516ed1ea688ab1a33f0a0dca8a491fe716df
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-04 05:22:58 +0900

    [OURS-413] feature: inject dependency `courseInBookmarkService`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/CourseInBookmarkApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/CourseInBookmarkApiController.java
@@ -1,8 +1,15 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in;
 
+import kr.yeoksi.ours.oursserver.course.service.port.in.CourseInBookmarkService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 public class CourseInBookmarkApiController {
+
+    private final CourseInBookmarkService courseInBookmarkService;
+
+
 }
