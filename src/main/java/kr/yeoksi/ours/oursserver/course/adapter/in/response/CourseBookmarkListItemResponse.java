4e59faa4136afc3a9762c7f24d96f0d9cb050b7e
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-05 03:30:16 +0900

    [OURS-432] feature: define `CourseBookmarkListItemResponse`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/response/CourseBookmarkListItemResponse.java
@@ -0,0 +1,11 @@
+package kr.yeoksi.ours.oursserver.course.adapter.in.response;
+
+import java.time.LocalDateTime;
+
+public class CourseBookmarkListItemResponse {
+
+    private Long id;
+    private String title;
+    private LocalDateTime createdAt;
+
+}
