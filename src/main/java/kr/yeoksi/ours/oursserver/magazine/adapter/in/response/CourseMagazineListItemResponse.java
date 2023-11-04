b04099a46cfdd9ee6ffdedfbb5aed89ce1d7a812
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-05 03:39:00 +0900

    [OURS-432] feature: define `CourseMagazineListItemResponse`

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/response/CourseMagazineListItemResponse.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/response/CourseMagazineListItemResponse.java
@@ -1,4 +1,21 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.in.response;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Builder
 public class CourseMagazineListItemResponse {
+
+    private Long id;
+
+    private String title;
+    private String contents;
+
+    private String imgUrl;
+
+    private LocalDateTime createdAt;
 }
