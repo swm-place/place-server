601db4b78014102143c8618157447a0d7c7cbe90
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-05 03:36:00 +0900

    [OURS-432] feature: `CourseBookmarkListItemResponse`에서 대표 사진 URI 하나만 응답

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/response/CourseBookmarkListItemResponse.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/response/CourseBookmarkListItemResponse.java
@@ -14,13 +14,25 @@ public class CourseBookmarkListItemResponse {
     private Long id;
     private String title;
     private LocalDateTime createdAt;
+    private String imgUrl;
 
 
     public static CourseBookmarkListItemResponse from(CourseBookmark courseBookmark) {
-        return CourseBookmarkListItemResponse.builder()
+        CourseBookmarkListItemResponse response = CourseBookmarkListItemResponse.builder()
                 .id(courseBookmark.getId())
                 .title(courseBookmark.getTitle())
                 .createdAt(courseBookmark.getCreatedAt())
                 .build();
+
+        if (!courseBookmark.getCoursesInBookmark().isEmpty()
+                && !courseBookmark.getCoursesInBookmark().get(0).getCourse().getPlacesInCourse().isEmpty()) {
+
+            response.setImgUrl(
+                    courseBookmark.getCoursesInBookmark().get(0)
+                            .getCourse().getPlacesInCourse().get(0)
+                            .getPlace().getImgUrl());
+        }
+
+        return response;
     }
 }
