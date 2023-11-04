b0d8b30a2d2eeb9be56c4cdc26e2b0ca379b155e
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-05 01:17:50 +0900

    [OURS-413] feature: implement `from()`

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/response/CourseBookmarkReferenceResponse.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/response/CourseBookmarkReferenceResponse.java
@@ -1,5 +1,6 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in.response;
 
+import kr.yeoksi.ours.oursserver.course.domain.CourseBookmark;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,5 +11,13 @@ public class CourseBookmarkReferenceResponse {
 
     private Long id;
     private String title;
+
+
+    public static CourseBookmarkReferenceResponse from(CourseBookmark courseBookmark) {
+        return CourseBookmarkReferenceResponse.builder()
+                .id(courseBookmark.getId())
+                .title(courseBookmark.getTitle())
+                .build();
+    }
 
 }
