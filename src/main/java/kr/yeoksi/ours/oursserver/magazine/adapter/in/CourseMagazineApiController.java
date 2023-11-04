01d8df0362b6b63e55b955052980c1396bd9a4b6
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-05 03:40:45 +0900

    [OURS-432] feature: `getLatestMagazines()` 시 `CourseMagazineListItemResponse` 기반 응답

--- src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineApiController.java
+++ src/main/java/kr/yeoksi/ours/oursserver/magazine/adapter/in/CourseMagazineApiController.java
@@ -1,6 +1,7 @@
 package kr.yeoksi.ours.oursserver.magazine.adapter.in;
 
 import kr.yeoksi.ours.oursserver.magazine.adapter.in.request.CourseMagazineRequest;
+import kr.yeoksi.ours.oursserver.magazine.adapter.in.response.CourseMagazineListItemResponse;
 import kr.yeoksi.ours.oursserver.magazine.adapter.in.response.CourseMagazineResponse;
 import kr.yeoksi.ours.oursserver.magazine.service.port.in.CourseMagazineService;
 import lombok.RequiredArgsConstructor;
@@ -18,13 +19,13 @@ public class CourseMagazineApiController {
 
 
     @GetMapping
-    public ResponseEntity<List<CourseMagazineResponse>> getLatestMagazines(@RequestParam(defaultValue = "0") int page,
-                                                                           @RequestParam(defaultValue = "10") int count) {
+    public ResponseEntity<List<CourseMagazineListItemResponse>> getLatestMagazines(@RequestParam(defaultValue = "0") int page,
+                                                                                            @RequestParam(defaultValue = "10") int count) {
         // TODO: 리스트에 맞는 필드만 제공하도록 수정
         // TODO: 타이틀 사진 혹은 플레이스 사진 연동 (또는 연동을 위한 가이드/레퍼런스 제공)
         return ResponseEntity.ok(
                 courseMagazineService.findLatestCourseMagazines(count, page)
-                        .stream().map(CourseMagazineResponse::from)
+                        .stream().map(CourseMagazineListItemResponse::from)
                         .toList());
     }
 
