116ab07e0e172d401a96b0720c18c49902784fc9
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-05 00:20:59 +0900

    [OURS-413] fix: `jackson`이 직렬화할 수 있도록 생성자 추가

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/request/CourseBookmarkUpdateRequest.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/request/CourseBookmarkUpdateRequest.java
@@ -1,12 +1,16 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in.request;
 
 import kr.yeoksi.ours.oursserver.course.domain.CourseBookmark;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CourseBookmarkUpdateRequest {
 
     private Long id;
