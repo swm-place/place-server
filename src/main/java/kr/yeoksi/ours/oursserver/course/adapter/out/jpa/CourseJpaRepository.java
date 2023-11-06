48a1950099d4e817ee2e13451a7011888e8d25fc
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-06 20:07:58 +0900

    [OURS-452] fix: `findAllByUserId()`에 `Pageable` 추가

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/jpa/CourseJpaRepository.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/out/jpa/CourseJpaRepository.java
@@ -1,12 +1,13 @@
 package kr.yeoksi.ours.oursserver.course.adapter.out.jpa;
 
 import kr.yeoksi.ours.oursserver.course.adapter.out.jpa.entity.CourseJpaEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CourseJpaRepository extends JpaRepository<CourseJpaEntity, Long> {
 
-    List<CourseJpaEntity> findAllByUserId(String userId);
+    List<CourseJpaEntity> findAllByUserId(String userId, Pageable pageable);
 
 }
