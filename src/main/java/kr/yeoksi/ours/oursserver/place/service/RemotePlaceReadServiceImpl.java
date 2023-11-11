dfccc161d985c4f3bdee6bab3398b5884960aa77
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 01:06:37 +0900

    [OURS-467] feature: inject `remotePlaceManager`

--- src/main/java/kr/yeoksi/ours/oursserver/place/service/RemotePlaceReadServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/place/service/RemotePlaceReadServiceImpl.java
@@ -2,11 +2,18 @@ package kr.yeoksi.ours.oursserver.place.service;
 
 import kr.yeoksi.ours.oursserver.place.domain.Place;
 import kr.yeoksi.ours.oursserver.place.service.port.in.RemotePlaceReadService;
+import kr.yeoksi.ours.oursserver.place.service.port.out.RemotePlaceManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@RequiredArgsConstructor
 public class RemotePlaceReadServiceImpl implements RemotePlaceReadService {
+
+    private final RemotePlaceManager remotePlaceManager;
+
+
     @Override
     public Place findById(String id) {
         return null;
