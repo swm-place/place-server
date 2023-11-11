fb2b8894b25b95541c568a2b4ae7c703156bf9a1
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 05:47:54 +0900

    [OURS-467] fix: define `NoArgsConstructor`, `AllArgsConstructor`, `JsonNaming` for jackson

--- src/main/java/kr/yeoksi/ours/oursserver/place/domain/GooglePlacePhotoAttribution.java
+++ src/main/java/kr/yeoksi/ours/oursserver/place/domain/GooglePlacePhotoAttribution.java
@@ -1,11 +1,18 @@
 package kr.yeoksi.ours.oursserver.place.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class GooglePlacePhotoAttribution {
 
     private String htmlAttribution;
