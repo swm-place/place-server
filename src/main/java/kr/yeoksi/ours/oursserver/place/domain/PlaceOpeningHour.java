0a54104504ca0562e03eaa372df4c3e5307048d6
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 05:47:33 +0900

    [OURS-467] fix: define `NoArgsConstructor`, `AllArgsConstructor`, `JsonNaming` for jackson

--- src/main/java/kr/yeoksi/ours/oursserver/place/domain/PlaceOpeningHour.java
+++ src/main/java/kr/yeoksi/ours/oursserver/place/domain/PlaceOpeningHour.java
@@ -1,10 +1,17 @@
 package kr.yeoksi.ours.oursserver.place.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PlaceOpeningHour {
 
     private Integer weekday;
