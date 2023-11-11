408a7aa18bfb0c1e9c2c6c4c08269e3d9b7a08ba
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 05:47:26 +0900

    [OURS-467] fix: define `NoArgsConstructor`, `AllArgsConstructor`, `JsonNaming` for jackson

--- src/main/java/kr/yeoksi/ours/oursserver/place/domain/PlaceReview.java
+++ src/main/java/kr/yeoksi/ours/oursserver/place/domain/PlaceReview.java
@@ -1,14 +1,21 @@
 package kr.yeoksi.ours.oursserver.place.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import kr.yeoksi.ours.oursserver.user.domain.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PlaceReview {
 
     private Long id;
