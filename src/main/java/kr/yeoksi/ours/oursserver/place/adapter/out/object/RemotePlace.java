7e37a3c3f5d654d2d46e52b822ca8ab81fc86997
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 02:47:49 +0900

    [OURS-467] feature: define `RemotePlace`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/place/adapter/out/object/RemotePlace.java
@@ -0,0 +1,41 @@
+package kr.yeoksi.ours.oursserver.place.adapter.out.object;
+
+import kr.yeoksi.ours.oursserver.common.domain.Location;
+import kr.yeoksi.ours.oursserver.place.domain.Place;
+import kr.yeoksi.ours.oursserver.place.domain.PlaceOpeningHour;
+import kr.yeoksi.ours.oursserver.place.domain.PlacePhotoRef;
+import kr.yeoksi.ours.oursserver.place.domain.PlaceReview;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+@Builder
+public class RemotePlace {
+
+    private String id;
+
+    private String name;
+    private String category;
+    private String summary;
+
+    private String address;
+    private String roadAddress;
+    private String phoneNumber;
+
+    private Location location;
+
+    private List<String> hashtags;
+
+    private List<PlaceOpeningHour> openingHours;
+    private List<String> openingHoursText;
+    private Boolean openNow;
+
+    private List<PlacePhotoRef> photos;
+    private List<PlaceReview> reviews;
+
+    private Boolean isFavorite;
+
+}
