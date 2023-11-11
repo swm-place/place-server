e951df255295a55b78ca680c9a81a9ddf4a0986e
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 01:05:41 +0900

    [OURS-467] feature: create `RemotePlaceReadServiceImpl`

---
+++ src/main/java/kr/yeoksi/ours/oursserver/place/service/RemotePlaceReadServiceImpl.java
@@ -0,0 +1,11 @@
+package kr.yeoksi.ours.oursserver.place.service;
+
+import kr.yeoksi.ours.oursserver.place.domain.Place;
+import kr.yeoksi.ours.oursserver.place.service.port.in.RemotePlaceReadService;
+
+public class RemotePlaceReadServiceImpl implements RemotePlaceReadService {
+    @Override
+    public Place findById(String id) {
+        return null;
+    }
+}
