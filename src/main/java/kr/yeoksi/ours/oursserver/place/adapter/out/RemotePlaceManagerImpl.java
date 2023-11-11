fbc440fe94cbc347f5d5f0549525eac1c012b09d
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 03:03:27 +0900

    [OURS-467] feature: implement `findById()`

--- src/main/java/kr/yeoksi/ours/oursserver/place/adapter/out/RemotePlaceManagerImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/place/adapter/out/RemotePlaceManagerImpl.java
@@ -1,9 +1,14 @@
 package kr.yeoksi.ours.oursserver.place.adapter.out;
 
+import kr.yeoksi.ours.oursserver.place.adapter.out.object.RemotePlace;
 import kr.yeoksi.ours.oursserver.place.domain.Place;
 import kr.yeoksi.ours.oursserver.place.service.port.out.RemotePlaceManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 
 @Component
@@ -11,9 +16,23 @@ public class RemotePlaceManagerImpl implements RemotePlaceManager {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Value("{internal.server.recommender.url}")
+    private String recommenderUrl;
+
 
     @Override
-    public Place findById(String id) {
-        return null;
+    public Optional<Place> findById(String id) {
+        Optional<RemotePlace> remotePlace;
+
+        try {
+            remotePlace = Optional.ofNullable(restTemplate.getForObject(
+                    String.format("http://%s/places/%s", this.recommenderUrl, id),
+                    RemotePlace.class));
+        } catch (HttpStatusCodeException e) {
+            return Optional.empty();
+        }
+
+        return remotePlace.map(RemotePlace::toPlace);
     }
+
 }
