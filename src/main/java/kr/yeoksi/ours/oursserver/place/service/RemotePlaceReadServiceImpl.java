c74e0351edbd7da49f163a4167469c49be81e034
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 01:06:02 +0900

    [OURS-467] feature: register service bean

--- src/main/java/kr/yeoksi/ours/oursserver/place/service/RemotePlaceReadServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/place/service/RemotePlaceReadServiceImpl.java
@@ -2,7 +2,10 @@ package kr.yeoksi.ours.oursserver.place.service;
 
 import kr.yeoksi.ours.oursserver.place.domain.Place;
 import kr.yeoksi.ours.oursserver.place.service.port.in.RemotePlaceReadService;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class RemotePlaceReadServiceImpl implements RemotePlaceReadService {
     @Override
     public Place findById(String id) {
