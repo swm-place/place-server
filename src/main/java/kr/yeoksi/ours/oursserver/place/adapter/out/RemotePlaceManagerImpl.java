d6d4bd7e13a2abccaa9b4cb24c78948c28d5517f
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 02:09:12 +0900

    [OURS-467] feature: define `restTemplate`

--- src/main/java/kr/yeoksi/ours/oursserver/place/adapter/out/RemotePlaceManagerImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/place/adapter/out/RemotePlaceManagerImpl.java
@@ -3,10 +3,15 @@ package kr.yeoksi.ours.oursserver.place.adapter.out;
 import kr.yeoksi.ours.oursserver.place.domain.Place;
 import kr.yeoksi.ours.oursserver.place.service.port.out.RemotePlaceManager;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 
 @Component
 public class RemotePlaceManagerImpl implements RemotePlaceManager {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+
     @Override
     public Place findById(String id) {
         return null;
