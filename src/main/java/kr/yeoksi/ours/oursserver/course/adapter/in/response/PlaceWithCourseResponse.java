24796bff03d4f31b81a6ffc54f0c9d53729cee47
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-12 04:07:54 +0900

    [OURS-467] feature: Place 추가 필드 반영 및 `from()` 구현

--- src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/response/PlaceWithCourseResponse.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/adapter/in/response/PlaceWithCourseResponse.java
@@ -1,15 +1,20 @@
 package kr.yeoksi.ours.oursserver.course.adapter.in.response;
 
-import kr.yeoksi.ours.oursserver.others.domain.*;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import kr.yeoksi.ours.oursserver.common.domain.Location;
+import kr.yeoksi.ours.oursserver.others.domain.Place;
+import kr.yeoksi.ours.oursserver.place.domain.PlaceOpeningHour;
+import kr.yeoksi.ours.oursserver.place.domain.PlacePhotoRef;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
-import java.util.Map;
 
 
 @Data
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PlaceWithCourseResponse {
 
     private String id;
@@ -18,6 +23,15 @@ public class PlaceWithCourseResponse {
     private String category;
     private String imgUrl;
 
+    private Location location;
+
+    private List<PlaceOpeningHour> openingHours;
+    private List<String> openingHoursText;
+    private Boolean openNow;
+
+    private List<PlacePhotoRef> photos;
+    private List<kr.yeoksi.ours.oursserver.place.domain.PlaceReview> reviews;
+
 
     public static PlaceWithCourseResponse from(Place place) {
         return PlaceWithCourseResponse.builder()
@@ -25,6 +39,21 @@ public class PlaceWithCourseResponse {
                 .name(place.getName())
                 .category(place.getCategory())
                 .imgUrl(place.getImgUrl())
+                .build();
+    }
+
+    public static PlaceWithCourseResponse from(kr.yeoksi.ours.oursserver.place.domain.Place place) {
+        return PlaceWithCourseResponse.builder()
+                .id(place.getId())
+                .name(place.getName())
+                .category(place.getCategory())
+                .imgUrl(place.getPhotos().get(0).getUrl())
+                .location(place.getLocation())
+                .openingHours(place.getOpeningHours())
+                .openingHoursText(place.getOpeningHoursText())
+                .openNow(place.getOpenNow())
+                .photos(place.getPhotos())
+                .reviews(place.getReviews())
                 .build();
     }
 }
