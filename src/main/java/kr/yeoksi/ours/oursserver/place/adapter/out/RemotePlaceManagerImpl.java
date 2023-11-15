d23edaa6bff6b8bd9db97c1be9344c3d4b7676e3
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-16 02:49:12 +0900

    [OURS-479] feature: restTemplate 로깅 로직 추가

--- src/main/java/kr/yeoksi/ours/oursserver/place/adapter/out/RemotePlaceManagerImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/place/adapter/out/RemotePlaceManagerImpl.java
@@ -3,6 +3,7 @@ package kr.yeoksi.ours.oursserver.place.adapter.out;
 import kr.yeoksi.ours.oursserver.place.adapter.out.object.RemotePlace;
 import kr.yeoksi.ours.oursserver.place.domain.Place;
 import kr.yeoksi.ours.oursserver.place.service.port.out.RemotePlaceManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 
 @Component
+@Slf4j
 public class RemotePlaceManagerImpl implements RemotePlaceManager {
 
     private final RestTemplate restTemplate = new RestTemplate();
@@ -30,6 +32,7 @@ public class RemotePlaceManagerImpl implements RemotePlaceManager {
                     String.format("%s/places/%s", this.recommenderUrl, id),
                     RemotePlace.class));
         } catch (HttpStatusCodeException | ResourceAccessException e) {
+            log.error("Failed to get place from recommender server. id: {}", id, e);
             return Optional.empty();
         }
 
