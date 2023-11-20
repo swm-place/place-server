fabc9660a927f595a5cee9fec5ff4ea89af068d2
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-21 00:31:24 +0900

    [OURS-504] fix: remove `coursePage`, `courseSize`

--- src/main/java/kr/yeoksi/ours/oursserver/course/service/CourseBookmarkServiceImpl.java
+++ src/main/java/kr/yeoksi/ours/oursserver/course/service/CourseBookmarkServiceImpl.java
@@ -31,7 +31,7 @@ public class CourseBookmarkServiceImpl implements CourseBookmarkService {
     }
 
     @Override
-    public CourseBookmark getCourseBookmark(Long courseBookmarkId, String userId, int coursePage, int courseSize) {
+    public CourseBookmark getCourseBookmark(Long courseBookmarkId, String userId) {
         CourseBookmark courseBookmark = courseBookmarkRepository.findById(courseBookmarkId)
                 .orElseThrow(NotExistedCourseBookmarkException::new);
 
@@ -40,17 +40,17 @@ public class CourseBookmarkServiceImpl implements CourseBookmarkService {
         }
 
         courseBookmark.setCoursesInBookmark(
-                courseInBookmarkRepository.findByCourseBookmarkId(courseBookmarkId, coursePage, courseSize));
+                courseInBookmarkRepository.findByCourseBookmarkId(courseBookmarkId));
 
         return courseBookmark;
     }
 
     @Override
-    public List<CourseBookmark> getMyCourseBookmarks(String userId, int page, int size, int coursePage, int courseSize) {
+    public List<CourseBookmark> getMyCourseBookmarks(String userId, int page, int size) {
         // TODO: 쿼리 여러 번 나가지 않고 한 번에 가져올 수 있도록 연관관계 동기화 문제 재점검
         return courseBookmarkRepository.findByUserId(userId, page, size).stream()
                 .peek(courseBookmark -> courseBookmark.setCoursesInBookmark(
-                        courseInBookmarkRepository.findByCourseBookmarkId(courseBookmark.getId(), coursePage, courseSize)))
+                        courseInBookmarkRepository.findByCourseBookmarkId(courseBookmark.getId())))
                 .toList();
     }
 
