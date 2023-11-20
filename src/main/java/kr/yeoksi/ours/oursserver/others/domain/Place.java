21e287b592b4f3bcd8fc4a4add6f0dd33450c77d
Coldot <41678750+Coldot@users.noreply.github.com>
2023-11-20 13:06:59 +0900

    [OURS-495] feature: implement `toOrgPlace()`

--- src/main/java/kr/yeoksi/ours/oursserver/others/domain/Place.java
+++ src/main/java/kr/yeoksi/ours/oursserver/others/domain/Place.java
@@ -5,6 +5,7 @@ import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import kr.yeoksi.ours.oursserver.place.domain.PlacePhotoRef;
 import lombok.*;
 import org.hibernate.annotations.Type;
 
@@ -69,5 +70,19 @@ public class Place {
     // 현재 장소 사진은 다른 서버에 의존하고 있어, 해당 서버로의 요청 URI를 포함
     public String getImgUrl() {
         return "/api-recommender/place-photo/place/" + id;
+    }
+
+    public kr.yeoksi.ours.oursserver.place.domain.Place toOrgPlace() {
+        List<PlacePhotoRef> photos = new ArrayList<>();
+        photos.add(PlacePhotoRef.builder()
+                .url(this.imgUrl)
+                .build());
+
+        return kr.yeoksi.ours.oursserver.place.domain.Place.builder()
+                .id(this.id)
+                .name(this.name)
+                .category(this.category)
+                .photos(photos)
+                .build();
     }
 }
